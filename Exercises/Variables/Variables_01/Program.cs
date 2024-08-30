04e592e43b0c94a8bbdfdb6c76fed422cc5e4598
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-08-30 17:54:41 +0200

    First exercise completed.

---
+++ Exercises/Variables/Variables_01/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Variables_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ange ditt namn: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Välkommen {userName}!");
+
+            Console.ReadKey();
+        }
+    }
+}

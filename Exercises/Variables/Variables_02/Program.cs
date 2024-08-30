93147c80f83fe269ae05cb29c2465f27cca70e3b
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-08-30 18:14:37 +0200

    Completed Exercise 02.

---
+++ Exercises/Variables/Variables_02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.CompilerServices;
+using System.Transactions;
+
+namespace Variables_02
+{
+    internal class Program
+    {
+        // 2. Multiplicera två tal
+        static void Main(string[] args)
+        {
+            string userInput;
+            int firstNumber;
+            int secondNumber;
+
+
+            userInput = GetUserInput("Ange första talet");
+
+            while (!Int32.TryParse(userInput, out firstNumber ))
+            {
+                userInput = GetUserInput("Vänligen ange ett heltal");
+            }
+
+            userInput = GetUserInput("Ange andra talet");
+
+            while (!Int32.TryParse(userInput, out secondNumber))
+            {
+                userInput = GetUserInput("Vänligen ange ett heltal");
+            }
+
+            Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+
+        }
+
+        private static string GetUserInput(string prompt)
+        {
+            Console.Write(prompt + " > ");
+            return Console.ReadLine();
+        }
+    }
+}

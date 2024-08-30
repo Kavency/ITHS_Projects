863eb8bcb7a3707f3c05344763f9e063e4776dcd
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-08-30 18:26:47 +0200

    Completed exercise 03.

---
+++ Exercises/Variables/Variables_03/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Variables_03
+{
+    internal class Program
+    {
+        // 3. Verifiera lösenord
+        static void Main(string[] args)
+        {
+            string passWord = "abc123";
+
+            Console.Write("Please enter your super secure password in plain text: ");
+            string userInput = Console.ReadLine();
+
+            while (passWord != userInput)
+            {
+                Console.WriteLine("Wrong password entered");
+                Console.Write("Enter your password: ");
+                userInput = Console.ReadLine();
+            }
+
+            Console.WriteLine("Correct!");
+            Console.ReadKey();
+        }
+    }
+}

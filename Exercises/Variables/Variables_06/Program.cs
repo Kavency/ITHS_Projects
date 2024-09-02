9695a96b1fd56491f69e258d310727b22948f413
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-02 10:56:13 +0200

    Completed exercise 06.

---
+++ Exercises/Variables/Variables_06/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Variables_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstResult;
+            double secondResult;
+
+            while (true)
+            {
+                Console.Write("Ange första talet: ");
+                string firstInput = Console.ReadLine();
+                
+                if (!double.TryParse(firstInput, out firstResult))
+                    continue;
+
+                Console.Write("Ange operation (+,-,*,/): ");
+                string operatorInput = Console.ReadLine();
+
+                if (operatorInput != "+" && operatorInput != "-" && operatorInput != "*" && operatorInput != "/")
+                    continue;
+                
+                Console.Write("Ange andra talet: ");
+                string secondInput = Console.ReadLine();
+
+                if (!double.TryParse(secondInput, out  secondResult))
+                    continue;
+
+                switch (operatorInput)
+                {
+                    case "+":
+                        Console.WriteLine($"{firstInput} {operatorInput} {secondInput} = {firstResult + secondResult}");
+                        break;
+                    case "-":
+                        Console.WriteLine($"{firstInput} {operatorInput} {secondInput} = {firstResult - secondResult}");
+                        break;
+                    case "*":
+                        Console.WriteLine($"{firstInput} {operatorInput} {secondInput} = {firstResult * secondResult}");
+                        break;
+                    case "/":
+                        Console.WriteLine($"{firstInput} {operatorInput} {secondInput} = {firstResult / secondResult}");
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+
+        }
+    }
+}

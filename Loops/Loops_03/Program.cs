067656c102005192f36edb86f48a0961f40a00a8
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-02 11:38:56 +0200

    Completed exercise 03.

---
+++ Loops/Loops_03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.Design;
+
+namespace Loops_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 30; i++)
+            {
+                if (i % 3 == 0)
+                    Console.WriteLine($"{i} är jämnt delbart med tre.");
+                else
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}

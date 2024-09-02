2f41705c2629e8fdcaf3be38c9944f56d1184d9d
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-02 11:45:55 +0200

    Completed exercise 04.

---
+++ Loops/Loops_04/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Loops_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ange ett tal: ");
+            string userInput = Console.ReadLine();
+
+            int.TryParse(userInput, out int result);
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(result * i);
+            }
+        }
+    }
+}

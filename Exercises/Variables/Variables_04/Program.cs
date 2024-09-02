9710824969100651eef256810f80497a75ea6d5f
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-02 09:35:17 +0200

    Completed exercise 04.

---
+++ Exercises/Variables/Variables_04/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Variables_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int compareToValue = 100;
+                
+                Console.Write("Ange ett tal (x för att avsluta): ");
+                string userInput = Console.ReadLine();
+
+                if (userInput.ToLower() == "x")
+                    break;
+
+                if (!Int32.TryParse(userInput, out int result))
+                    continue;
+
+                if (result > compareToValue)
+                    Console.WriteLine($"Talet är större än {compareToValue}!");
+                else if (result < compareToValue)
+                    Console.WriteLine($"Talet är mindre än {compareToValue}!");
+                else
+                    Console.WriteLine($"Talet är exakt {compareToValue}!");
+            }
+
+
+        }
+    }
+}

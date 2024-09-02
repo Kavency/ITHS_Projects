b238d7b1465b89ecf920e5a66cc85e09d4a57c42
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-02 09:58:33 +0200

    Completed exercise 5.

---
+++ Exercises/Variables/Variables_05/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Variables_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.Write("Ange ett tal: ");
+                string userInput = Console.ReadLine();
+
+                if (userInput.ToLower() == "x")
+                    break;
+
+                if (!double.TryParse(userInput, out double result))
+                    continue;
+
+                Console.WriteLine($"{result * 2} är dubbelt så mycket som {result}");
+                Console.WriteLine($"{result / 2} är hälften så mycket som {result}");
+            }
+
+        }
+    }
+}

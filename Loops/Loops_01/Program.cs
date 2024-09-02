405dd7f27563b40ca70bb2a262452f93e52cb6fd
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-02 11:31:39 +0200

    Completed exercise 01.

---
+++ Loops/Loops_01/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Loops_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 20; i <= 30; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}

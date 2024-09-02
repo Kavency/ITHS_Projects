6ca9c4a39316e6fa35b087d09df0c78d15008680
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-02 14:03:16 +0200

    Completed exercise 06.

---
+++ Loops/Loops_06/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Loops_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal sum = 1;
+
+            for (int i = 0; i <= 64; i++)
+            {
+                Console.WriteLine(sum *= 2);
+            }
+        }
+    }
+}

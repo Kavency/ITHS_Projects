7d489ee8efb0b936caa78a44f1c2db1742ca5b84
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-02 15:38:41 +0200

    Completed exercise 09.

---
+++ Exercises/Loops/Loops_09/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Loops_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = 0;
+            int height = 0;
+            int evenUnevenToggle = 1;
+
+            do
+            {
+                Console.Write("Ange bredd: ");
+            } while (!Int32.TryParse(Console.ReadLine(), out width));
+
+            do
+            {
+                Console.Write("Ange höjd: ");
+            } while (!Int32.TryParse(Console.ReadLine(), out height));
+
+
+            for (int i = 1; i <= height; i++)
+            {
+                for (int j = 1; j <= width; j++)
+                {
+                    if (evenUnevenToggle > 0)
+                    {
+                        Console.Write("O");
+                    }
+                    else
+                        Console.Write("X");
+
+                    evenUnevenToggle *= -1;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}

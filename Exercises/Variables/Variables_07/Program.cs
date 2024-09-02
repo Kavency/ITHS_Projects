f23e9fa5d1386853ec67d0da9330948b9a67041d
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-02 11:25:10 +0200

    Completed exercise 07.

---
+++ Exercises/Variables/Variables_07/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Variables_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfInputs = 0;
+            double total = 0;
+
+            do
+            {
+                Console.Write("Mata in ett värde: ");
+                string userInput = Console.ReadLine();
+
+                if (double.TryParse(userInput, out double result))
+                {
+                    total += result;
+                    numberOfInputs++;
+                    Console.WriteLine($"Summa = {total}");
+                }
+                else if (userInput == "")
+                {
+                    Console.WriteLine($"Medelvärde = {total / numberOfInputs}");
+                    break;
+                }
+                else
+                    continue;
+
+            } while (true);
+        }
+    }
+}

56f6729fcaee2e5d435f8365efe3f78d03e5340e
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-02 12:54:13 +0200

    Completed exercise 05.

---
+++ Loops/Loops_05/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Loops_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = 1000;
+            int total = 0;
+            
+            for (int i = 0; i <= maxNumber; i++)
+            {
+                total += i;
+            }
+            
+            Console.WriteLine(total);
+
+        }
+    }
+}

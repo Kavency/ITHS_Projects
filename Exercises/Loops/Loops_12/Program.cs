a8e82066b394af858ecf129847c287fa42bd8138
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-02 17:49:35 +0200

    First commit.

---
+++ Exercises/Loops/Loops_12/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Loops_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numbers = "";
+            int lineNumber = 1;
+
+            for (int i = 1; i < 10; i++)
+            {
+                numbers += i.ToString();
+                Console.WriteLine(numbers);
+            }
+        }
+    }
+}

4760a92cf47e25092565bdb3ad36d87d3c45d099
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-02 15:28:32 +0200

    Completed exercise 08.

---
+++ Exercises/Loops/Loops_08/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Loops_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = 0;
+            int height = 0;
+
+            do
+            {
+                Console.Write("Ange bredd: ");
+            } while (!Int32.TryParse(Console.ReadLine(), out width));
+
+            do
+            {
+                Console.Write("Ange höjd: ");
+            } while (!Int32.TryParse(Console.ReadLine(), out height));
+
+            for (int i = 1; i <= height; i++)
+            {
+                for (int j = 1; j <= width; j++)
+                {
+                    if (j % 2 == 0)
+                    {
+                        Console.Write("O");
+                    }
+                    else
+                        Console.Write("X");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}

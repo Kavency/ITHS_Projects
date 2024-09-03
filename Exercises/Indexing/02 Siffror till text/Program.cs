fe3c1e7a392a3fdeafba757dd40d5c971f9db4f0
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-03 10:25:53 +0200

    First commit.

---
+++ Exercises/Indexing/02 Siffror till text/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02_Siffror_till_text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

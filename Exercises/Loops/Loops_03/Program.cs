c652d63ff7cbf66f90818eac70cb712c49a2db92
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-03 15:13:31 +0200

    Changed project name and added a comment.

--- Exercises/Loops/Loops_03/Program.cs
+++ Exercises/Loops/Loops_03/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.Design;
+﻿// 3.Var tredje "Hej"
+// Skriv en loop som skriver ut tal från 0 till 30, 
+// men om talet är jämnt delbart med 3 skriv istället “Hej”.
 
 namespace Loops_03
 {

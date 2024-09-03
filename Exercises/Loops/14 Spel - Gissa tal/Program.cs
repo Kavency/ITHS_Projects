004131b52d1c6bda2dcbfda91e83bbc84d4708b3
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-03 18:22:07 +0200

    Initial commit.

---
+++ Exercises/Loops/14 Spel - Gissa tal/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _14_Spel___Gissa_tal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till gissa talet");
+            Console.WriteLine("--------------------------");
+
+
+        }
+    }
+}

b992d4ada0590f50fa87beee8b78740f3caf5720
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-03 15:17:54 +0200

    Changed project name and added a comment.

--- Exercises/Loops/Loops_06/Program.cs
+++ Exercises/Loops/Loops_06/Program.cs
@@ -1,4 +1,19 @@
-﻿namespace Loops_06
+﻿// 6.Riskorn på shackbräde
+// Enligt legenden så fick den man som hade konstruerat 
+// det formidabla spelet schack önska sig något värdefullt 
+// av den överförtjuste kungen. Monarken blev förvånad över 
+// uppfinnarens anspråkslöshet när gåvan skulle bestå av riskorn.
+
+// Antalet riskorn bestämdes enligt följande regel: ett korn 
+// skulle lämnas för första rutan på ett schackbräde, 
+// två korn för andra rutan, fyra för tredje, 
+// åtta för fjärde och så ändå till den sista rutan.
+
+// Skriv ett program som hjälper kungen att beräkna 
+// antalet riskorn på vardera ruta.
+
+
+namespace Loops_06
 {
     internal class Program
     {

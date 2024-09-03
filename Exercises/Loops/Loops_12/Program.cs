af671417f1ac0ec83271aca92f3b16df92d1555d
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-03 15:24:49 +0200

    Changed project name and added a comment.

--- Exercises/Loops/Loops_12/Program.cs
+++ Exercises/Loops/Loops_12/Program.cs
@@ -1,4 +1,9 @@
-﻿namespace Loops_12
+﻿// 12.Nio sifferpyramider
+// Gör ett program som skriver ut “pyramider” som i uppgiften ovan,
+// den första ska vara med en rad, följt av en pyramid med 2 rader,
+// sen 3 rader etc. ända upp till 9 rader i den sista pyramiden.
+
+namespace Loops_12
 {
     internal class Program
     {

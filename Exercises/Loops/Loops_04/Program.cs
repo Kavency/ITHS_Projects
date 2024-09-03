155d2e370f6e0a36c2082268ffc65a0c8bce3519
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-03 15:14:42 +0200

    Changed project name and added a comment.

--- Exercises/Loops/Loops_04/Program.cs
+++ Exercises/Loops/Loops_04/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace Loops_04
+﻿// 4.Gångertabell
+// Gör ett program som ber användaren mata in ett tal, 
+// skriv ut det talets “gångertabell” dvs, multiplikationer av talet med alla tal från 1 t.o.m. 10.
+
+namespace Loops_04
 {
     internal class Program
     {

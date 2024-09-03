f182852d1e401cb75856bb823174b162db7e522f
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-03 18:23:39 +0200

    Added description.

--- Exercises/Loops/14 Spel - Gissa tal/Program.cs
+++ Exercises/Loops/14 Spel - Gissa tal/Program.cs
@@ -1,4 +1,16 @@
-﻿namespace _14_Spel___Gissa_tal
+﻿// 14.Spel - Gissa tal
+// Gör ett gissa-spel som har ett hemligt nummer (lagrat i en variabel i koden) mellan 1 och 100. När spelet startar
+// ombeds spelaren att gissa ett tal mellan 1-100. Gissar man för lågt skriver programmet ut att man gissat för lågt,
+// och man får gissa igen. Gissar man för högt skriver programmet att man gissat för högt och man får gissa igen. Gissar
+// man rätt så skriver programmet att man gissat rätt, samt antal gissningar som krävdes för att gissa rätt.
+
+// Extrauppgift: Klassen “Random” i .NET kan användas för att sätta den hemliga variabeln till ett slumpmässigt 
+// tal 1-100. Googla “C# Random” och se om du får det att fungera så det blir olika tal varje gång man kör programmet.
+
+// Extrauppgift 2: Gör så att datorn själv (automatiskt) spelar spelet, och gissar tal. Skriv ut output precis som 
+// innan så man kan följa hur den spelat. Försök få den att lösa det så effektivt (så få drag) som möjligt.
+
+namespace _14_Spel___Gissa_tal
 {
     internal class Program
     {

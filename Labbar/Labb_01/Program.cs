57be7cf07191fb04f43bcd51b07fdb490f6e368e
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-05 16:04:17 +0200

    Added a header.

--- Labbar/Labb_01/Program.cs
+++ Labbar/Labb_01/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("********* Labb 01 *********");
+Console.WriteLine("* .NET24 - Magnus Hellman *");
+Console.WriteLine("***************************");

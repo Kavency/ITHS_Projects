332985dcd355ffbef99c7d1da613a3eb25cf6615
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-05 16:19:44 +0200

    Created GetUserInput method.

--- Labbar/Labb_01/Program.cs
+++ Labbar/Labb_01/Program.cs
@@ -1,3 +1,21 @@
 ﻿Console.WriteLine("********* Labb 01 *********");
 Console.WriteLine("* .NET24 - Magnus Hellman *");
 Console.WriteLine("***************************");
+Console.WriteLine();
+Console.WriteLine();
+
+
+
+
+
+
+static string GetUserInput(string prompt)
+{
+    Console.Write(prompt);
+    string input = Console.ReadLine();
+
+    if (input == string.Empty)
+        return "29535123p48723487597645723645";
+
+    return input;
+}

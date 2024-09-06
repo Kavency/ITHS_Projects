5ba28a08e767c091ae9997175c2663a5868c18fc
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-06 08:11:35 +0200

    Some minor code changes.

--- Labbar/Labb_01/Program.cs
+++ Labbar/Labb_01/Program.cs
@@ -1,50 +1,66 @@
-﻿Console.Clear();
+﻿using System.Xml.Linq;
+
+Console.Clear();
 Console.WriteLine("********* Labb 01 *********");
 Console.WriteLine("* .NET24 - Magnus Hellman *");
 Console.WriteLine("***************************");
 Console.WriteLine();
 Console.WriteLine();
 
-List<string> listOfSubstrings = new();
+List<string> listOfColoredNumbers = new();
+List<string> listOfSubStrings = new();
 
 long totalSum = 0;
 int startInnerLoopAt = 0;
 char charToCompare = ' ';
-string tempString = string.Empty;
-string stringToCheck = GetUserInput("Enter a string: ");
+string coloredNumbers = string.Empty;
+string subString = string.Empty;
 
+
+string stringToCheck = GetUserInput("Enter a string: ");
 
 for (int i = 0; i < stringToCheck.Length; i++)
 {
     charToCompare = stringToCheck[i];
-    tempString = string.Empty;
+    coloredNumbers = string.Empty;
 
     for (int j = startInnerLoopAt; j < stringToCheck.Length; j++)
     {
         if (char.IsDigit(stringToCheck[j]))
         {
-            tempString += stringToCheck[j];
+            coloredNumbers += stringToCheck[j];
 
-            if (charToCompare == stringToCheck[j] && tempString.Length > 1)
+            if (charToCompare == stringToCheck[j] && coloredNumbers.Length > 1)
             {
-                listOfSubstrings.Add(tempString);
+                listOfColoredNumbers.Add(coloredNumbers);
+
+                //for (int k = 0; k < coloredNumbers.Length; k++)
+                //{
+                //    Console.ForegroundColor = ConsoleColor.Green;
+                //    Console.Write(coloredNumbers[k]);
+                //    Console.ForegroundColor = ConsoleColor.Gray;
+                //    continue;
+                //}
+                subString = stringToCheck.Substring(i, coloredNumbers.Length);
                 break;
             }
         }
         else
             break;
     }
-
+    Console.WriteLine(subString);  // Skriver ut dubbelt när den stöter på en bokstav.
     startInnerLoopAt++;
 }
 
-
-foreach (var item in listOfSubstrings)
+foreach (string item in listOfColoredNumbers)
 {
     totalSum += long.Parse(item);
 }
 
+// Todo: OM listOfColoredNumbers är tom. Skriv ut stringToCheck en gång i grått.
 
+
+Console.WriteLine();
 Console.WriteLine($"Den totala summan är: {totalSum}");
 Console.ReadKey();
 

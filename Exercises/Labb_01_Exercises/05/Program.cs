ed7279830fb4741f80302fab62c285e57f4e02cf
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-06 15:29:14 +0200

    Some code changes.

--- Exercises/Labb_01_Exercises/05/Program.cs
+++ Exercises/Labb_01_Exercises/05/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace _05
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace _05
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = "Hello world";
+
+            //for (int i = 0; i < text.Length; i++)
+            foreach(char item in text)
+            {
+                Console.Write(item);
+
+
+
+                //if (i > 0 && text[i] == text[i - 1])
+                //{
+                //    Console.ForegroundColor = ConsoleColor.Green;
+                //    Console.Write(text[i - 1]);
+                //    Console.Write(text[i]);
+                //}
+                //else
+                //{
+                //    Console.ForegroundColor = ConsoleColor.Gray;
+                //    Console.Write(text[i]);
+                //}
+
+            }
         }
     }
 }

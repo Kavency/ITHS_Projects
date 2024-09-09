17dd7946ac3fdaf291d07b75898cea9b60f0ea40
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-09 10:11:44 +0200

    Refactoring. Rewrote the if statement and got rid of the second if.

--- Exercises/Labb_01_Exercises/05/Program.cs
+++ Exercises/Labb_01_Exercises/05/Program.cs
@@ -4,14 +4,14 @@
     {
         static void Main(string[] args)
         {
-            string text = "Hello world!!!";
+            string text = "Hello world!!!otkdw88maGG";
 
-            for (int i = 0; i < text.Length - 1; i++)
+            for (int i = 0; i < text.Length ; i++)
             {
                 if (i > 0 && text[i] == text[i - 1])  // Kontrollera bokstaven innan index.
                     Console.ForegroundColor = ConsoleColor.Green;
                 
-                else if (text[i] == text[i + 1]) // Kontrollera bokstaven efter index.
+                else if (text[i] == text[i + 1] && i != text.Length) // Kontrollera bokstaven efter index.
                     Console.ForegroundColor = ConsoleColor.Green;
                 
                 else
@@ -21,18 +21,19 @@
 
                 // Kontrollera om vi bifinner oss på näst sista index.
                 // Om så, kontrollera och skriv ut nästa bokstav, bryt sedan loopen.
-                if (i == text.Length - 2)
-                {
-                    if (text[i] == text[i + 1])
-                        Console.ForegroundColor = ConsoleColor.Green;
+                //if (i == text.Length - 2)
+                //{
+                //    if (text[i] == text[i + 1])
+                //        Console.ForegroundColor = ConsoleColor.Green;
 
-                    else
-                        Console.ForegroundColor = ConsoleColor.Gray;
-                    
-                    Console.WriteLine(text[i + 1]);
-                    Console.ForegroundColor = ConsoleColor.Gray;
-                    break;
-                }
+                //    else
+                //        Console.ForegroundColor = ConsoleColor.Gray;
+
+                //    Console.WriteLine(text[i + 1]);
+                //    
+                //    break;
+                //}
+                Console.ForegroundColor = ConsoleColor.Gray;
             }
         }
     }

159fb7d2d83b348cce974b166f1a4884777b8693
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-09 14:20:40 +0200

    Completed exercise 08. Not checking for cases in string.

--- Exercises/Labb_01_Exercises/08/Program.cs
+++ Exercises/Labb_01_Exercises/08/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = "How much wood would a woodchuck chuck if a woodchuck could chuck wood?";
+
+            Console.Write("Enter a searchstring: ");
+            string userInput = Console.ReadLine();
+
+            if (text.Contains(userInput))
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    int subStringIndex = text.IndexOf(userInput, i);
+
+                    if (i == subStringIndex)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(userInput);
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        i += userInput.Length - 1;
+                    }
+                    else
+                        Console.Write(text[i]);
+                }
+            }
+            else
+                Console.WriteLine("Could not find your searchstring.");
         }
     }
 }

ceec1edbc9ddf525087d7a201d940730e3b78d37
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-09 10:28:05 +0200

    Initial commit exercise 07

---
+++ Exercises/Labb_01_Exercises/07/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "How much wood would a woodchuck chuck if a woodchuck could chuck wood?";
+            string subString = "chuck";
+
+
+
+            if (text.Contains(subString))
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    int subStringIndex = text.IndexOf(subString, i);
+
+                    Console.Write(i);
+                }
+            }
+        }
+    }
+}

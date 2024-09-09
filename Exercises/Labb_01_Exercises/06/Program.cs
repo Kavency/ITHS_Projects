c869c5b50c46f41dcf837b3e5414613e47ea23e8
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-09 10:17:33 +0200

    Completed exercise 06.

--- Exercises/Labb_01_Exercises/06/Program.cs
+++ Exercises/Labb_01_Exercises/06/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = "How much wood would a woodchuck chuck if a woodchuck could chuck wood?";
+            string subString = "oo";
+
+            Console.WriteLine("How much wood would a woodchuck chuck if a woodchuck could chuck wood?");
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == 'o' && text[i + 1] == 'o' && i != text.Length)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(subString);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    i += 1;
+                }
+                else
+                    Console.Write(text[i]);
+            }
         }
     }
 }

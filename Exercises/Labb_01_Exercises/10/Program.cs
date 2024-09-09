2e94ab019e95b846166a11ab13773501c330e938
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-09 20:51:00 +0200

    Completed exrecise 10. Not the fanciest solution.

---
+++ Exercises/Labb_01_Exercises/10/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            string text = "How much wood would a woodchuck chuck if a woodchuck could chuck wood?";
+            int subStringIndex = 0;
+            int noOfPrintsOut = 1;
+
+            for (int i = 0; i <= text.Length; i++)
+            {
+                subStringIndex = text.IndexOf(" ", 0);
+
+                for (int j = 0; j < noOfPrintsOut; j++)
+                {
+                    Console.Write(text.Substring(0, subStringIndex + 1));
+                }
+
+                Console.WriteLine();
+                text = text.Remove(0, subStringIndex + 1);
+                noOfPrintsOut++;
+            }
+
+            for (int j = 0; j < noOfPrintsOut; j++)
+            {
+                Console.Write(text + " ");
+            }
+        }
+    }
+}

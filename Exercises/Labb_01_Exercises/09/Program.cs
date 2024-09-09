4a57e86b976e1297e27554c83199eb13d5fcc839
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-09 14:34:58 +0200

    Completed exercise 09.

---
+++ Exercises/Labb_01_Exercises/09/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello, World!";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.Write(text[i]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

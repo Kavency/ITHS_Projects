5f7a188c3e706e54c671f3d2ee7b3ef7ec4a71ec
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-09 21:07:36 +0200

    Initial commit exercise 12.

---
+++ Exercises/Labb_01_Exercises/12/Program.cs
@@ -0,0 +1,16 @@
+﻿// 12.Rödmarkerade ord.
+// Utgå från samma sträng igen, men denna gång ska programmet skriva ut hela strängen på varje rad.
+// På första raden ska första ordet vara rött (övriga vita), på andra raden ska andra ordet vara rött.
+// På tredje raden ska tredje vara rött osv.
+
+
+namespace _12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

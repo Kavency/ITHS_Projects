84b282a0a9dd4c46b4a8c7343ff06456a127bdbb
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-10 14:13:01 +0200

    First commit exercise 15.

---
+++ Exercises/Labb_01_Exercises/15/Program.cs
@@ -0,0 +1,17 @@
+﻿// 15.Rödmarkerade bokstäver 3
+// Användaren matar in valfri sträng. Programmet skriver sedan ut en hela strängen flera gånger.
+// På första raden börjar den skriva med röd färg på första bokstaven och fram tills den
+// hittar samma bokstav igen. På andra raden börjar den skriva med röd färg på andra bokstaven
+// och fram tills den hittar samma bokstav igen. På tredje raden börjar den skriva med röd text
+// på tredje bokstaven och fram tills den hittar samma igen... etc.
+
+namespace _15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = string.Empty;
+        }
+    }
+}

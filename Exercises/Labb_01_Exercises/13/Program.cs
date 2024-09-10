13214123666ea657b1827cb12951ae5033ea556d
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-10 10:42:44 +0200

    Initial commit exrecise 13.

---
+++ Exercises/Labb_01_Exercises/13/Program.cs
@@ -0,0 +1,16 @@
+﻿// 13.Rödmarkerade bokstäver
+// Utgå från strängen "abcdefghijklmnopqrstuvwxyz". Hela strängen skrivs ut på varje rad. 
+// På första raden är de 5 första tecknen röda. På nästa rad är “bcdef” röda, sedan “cedfg” osv.. 
+// alltså alltid 5 tecken, men de flyttas ett steg åt höger för varje rad, tills sista raden har “vwxyz” 
+// rödfärgad.
+
+namespace _13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

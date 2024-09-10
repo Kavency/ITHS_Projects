248666984cb35b102287ad454438f477ead33af9
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-10 11:48:06 +0200

    Completed exercise 13.

--- Exercises/Labb_01_Exercises/13/Program.cs
+++ Exercises/Labb_01_Exercises/13/Program.cs
@@ -10,7 +10,26 @@ namespace _13
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = "abcdefghijklmnopqrstuvwxyz";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < text.Length; j++)
+                {
+                    // j behöver vara lika med eller större än i för
+                    // att skjuta fram den röda texten ett steg varje iteration.
+                    if (j >= i && j < i + 5) 
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    
+                    else
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                    
+                    Console.Write(text[j]);
+                }
+                Console.WriteLine();
+            }
+            
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
     }
 }

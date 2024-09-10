fa9e0dc5e03d05797b16de988b52c47a0e9bccdc
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-10 07:55:08 +0200

    Fixed a out of bounds error on the IF ELSE statement.

--- Exercises/Labb_01_Exercises/05/Program.cs
+++ Exercises/Labb_01_Exercises/05/Program.cs
@@ -4,14 +4,14 @@
     {
         static void Main(string[] args)
         {
-            string text = "Hello world!!!otkdw88maGG";
+            string text = "Hello Woooorld!";
 
             for (int i = 0; i < text.Length ; i++)
             {
                 if (i > 0 && text[i] == text[i - 1])  // Kontrollera bokstaven innan index.
                     Console.ForegroundColor = ConsoleColor.Green;
                 
-                else if (text[i] == text[i + 1] && i != text.Length) // Kontrollera bokstaven efter index.
+                else if (i != text.Length - 1 && text[i] == text[i + 1]) // Kontrollera bokstaven efter index.
                     Console.ForegroundColor = ConsoleColor.Green;
                 
                 else

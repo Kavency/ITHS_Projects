0a5ae3e62cc092625f72a83dfadf6690db4f6447
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-10 11:54:01 +0200

    Initial commit exercise 14,

---
+++ Exercises/Labb_01_Exercises/14/Program.cs
@@ -0,0 +1,26 @@
+﻿// 14.Rödmarkerade bokstäver 2
+// Användaren matar in valfri sträng. Programmet skriver ut hela strängen
+// bokstav för bokstav. Alla bokstäverna är röda fram tills du stöter på ett 
+// tecken som är samma som första tecknet i strängen. Därefter skriver den ut
+// resten av bokstäverna med vit/grå färg.
+
+namespace _14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = string.Empty;
+            
+            Console.Write("Enter a string:> ");
+            userInput = Console.ReadLine();
+
+            // Skriv ut sträng bokstav för bokstav.
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                Console.Write(userInput[i]);
+            }
+
+        }
+    }
+}

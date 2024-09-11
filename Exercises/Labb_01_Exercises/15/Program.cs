f65680cf5f6d83f39b8fe184302af8ce89084104
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-11 11:07:19 +0200

    Completed exercise 15.

--- Exercises/Labb_01_Exercises/15/Program.cs
+++ Exercises/Labb_01_Exercises/15/Program.cs
@@ -12,16 +12,30 @@ namespace _15
         static void Main(string[] args)
         {
             string userInput = string.Empty;
+            int nextCharIndex = 0;
 
             Console.Write("Enter a string: ");
             userInput = Console.ReadLine();
 
             for (int i = 0; i < userInput.Length; i++)
             {
-                for (int j = 0; j < userInput.Length; j++)
+                nextCharIndex = userInput.IndexOf(userInput[i], i + 1);
+
+                for (int j = 0; j < userInput.Length && nextCharIndex >= 0; j++)
                 {
+                    if (j < nextCharIndex + 1 && j > i - 1)
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                    else
+                        Console.ForegroundColor = ConsoleColor.Gray;
+
                     Console.Write(userInput[j]);
                 }
+
+                if (nextCharIndex < 0)
+                    continue;
+
+                Console.ForegroundColor = ConsoleColor.Gray;
                 Console.WriteLine();
             }
         }

da42602fa3d31113327a827ab4649ad31306ac44
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-11 19:35:39 +0200

    Completed exercise 3.

--- Exercises/Indexing/03 Baklänges/Program.cs
+++ Exercises/Indexing/03 Baklänges/Program.cs
@@ -1,3 +1,10 @@
 ﻿// 3.Baklänges
 // Be användaren mata in en text. Skriv sedan ut texten baklänges.
 
+Console.Write("Enter any text: ");
+string userInput = Console.ReadLine();
+
+for(int i = userInput.Length - 1; i >= 0; i--)
+{
+    Console.Write(userInput[i]);
+}

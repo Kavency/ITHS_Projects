0c1155f19029c10ed620d5e4411ec200a42d8017
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-16 14:04:30 +0200

    Add description.

--- Exercises/OOP_Basics/02 Metod returnerar namnet/Program.cs
+++ Exercises/OOP_Basics/02 Metod returnerar namnet/Program.cs
@@ -1,10 +1,13 @@
-﻿Person kalle = new Person() { FirstName = "Kalle", LastName = "Anka" };
+﻿// 2.Lägg till en metod som returnerar hela namnet
+// Uppdatera klassen i uppgift 1 med en metod: 
+// public string GetFullName(), som returnerar en 
+// sträng med hela namnet.
+
+Person kalle = new Person() { FirstName = "Kalle", LastName = "Anka" };
 Person kajsa = new Person() { LastName = "Kajsa", FirstName = "Anka" };
 
 Console.WriteLine(kalle.GetFullName());
 Console.WriteLine(kajsa.GetFullName());
-
-
 
 class Person
 {

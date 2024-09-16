e6de6c5d7072e1405851fb0f556d3ca32747448d
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-16 14:44:55 +0200

    First commit.

---
+++ Exercises/OOP_Basics/06 Representerar dig och dina föräldrar/Program.cs
@@ -0,0 +1,53 @@
+﻿// 6.Skapa ett objekt som representerar dig och dina föräldrar
+// Gör en instans av en person och ge den ditt för- och efternamn.
+// Lägg även in dina föräldrars namn, kopplade till dig. Så t.ex myself.
+// GetFullName() returnerar ditt namn, och myself.mother.GetFullName() 
+// returnerar din mammas namn.
+
+// 5.Lägg till så personer kan ha föräldrar.
+// Uppdatera person-klassen så att varje person kan ha en mamma och en pappa.
+// Dessa representeras som publika fält(fields) av typen Person (eller vad du
+// kallade klassen i uppgift 1).
+
+// 4.Overload av GetFullName med titel.
+// GetFullName ska finnas i två versioner: en utan parametrar som i uppgift 2; 
+// och en där man kan skicka in en titel, t.ex. "Dr." eller "Mr." 
+// som returnerar namnet på personen med titeln framför.
+
+Person kalle = new Person() { FirstName = "Kalle", LastName = "Anka" };
+Person kajsa = new Person() { LastName = "Kajsa", FirstName = "Anka" };
+
+Console.WriteLine(kalle.ReturnFullNameReversed());
+
+class Person
+{
+    public string FirstName;
+    public string LastName;
+
+    public Person Mother;
+    public Person Father;
+
+    public string GetFullName()
+    {
+        return $"{this.FirstName} {this.LastName}";
+    }
+
+    public string GetFullName(string titel)
+    {
+        return $"{titel} {this.FirstName} {this.LastName}";
+    }
+
+    public string ReturnFullNameReversed()
+    {
+        string fullName = GetFullName();
+        string fullNameReversed = string.Empty;
+
+        for (int i = fullName.Length - 1; i >= 0; i--)
+        {
+            fullNameReversed += fullName[i];
+        }
+
+        return fullNameReversed;
+    }
+
+}

1231e65f6ca64ed4bcefd46eebbeb1a7e15dafce
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-16 19:19:02 +0200

    First commit, project 09.

---
+++ Exercises/OOP_Basics/09 Vikt och BMI/Program.cs
@@ -0,0 +1,5 @@
+﻿// 9.Vikt och BMI
+// Gör motsvarande för Vikt, alltså ett privat fält med publika metoder: double GetWeight()
+// och void SetWeight(double weight). Gör sedan även en publik metod double GetBMI()
+// som returnerar personens BMI. Detta ska dock inte finnas representerat som en private field.
+

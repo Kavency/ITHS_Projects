49b1d6802a953225e5f01b4f4ed49df21ca6ba48
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-16 13:16:10 +0200

    First commit.

---
+++ Exercises/OOP_Basics/1 Skriva ut namn på personer/Program.cs
@@ -0,0 +1,13 @@
+﻿// 1.Skriva ut namn på personer
+// Skriv en klass som representerar en person. Klassen ska ha två publika 
+// fält (fields): firstName, lastName. Skapa två instanser av personer med
+// olika för och efternamn, och använd sedan dessa för att printa ut (hela) 
+// namnen på de olika personerna.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}

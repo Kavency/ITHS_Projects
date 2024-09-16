e499b78ce647097bd3706a968b69d9184c833e4c
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-16 14:10:17 +0200

    First commit.

---
+++ Exercises/OOP_Basics/04 Overload av GetFullName/Program.cs
@@ -0,0 +1,34 @@
+﻿// 4.Overload av GetFullName med titel.
+// GetFullName ska finnas i två versioner: en utan parametrar som i uppgift 2; 
+// och en där man kan skicka in en titel, t.ex. "Dr." eller "Mr." 
+// som returnerar namnet på personen med titeln framför.
+
+Person kalle = new Person() { FirstName = "Kalle", LastName = "Anka" };
+Person kajsa = new Person() { LastName = "Kajsa", FirstName = "Anka" };
+
+Console.WriteLine(kalle.ReturnFullNameReversed());
+
+class Person
+{
+    public string FirstName;
+    public string LastName;
+
+    public string GetFullName()
+    {
+        return $"{this.FirstName} {this.LastName}";
+    }
+
+    public string ReturnFullNameReversed()
+    {
+        string fullName = GetFullName();
+        string fullNameReversed = string.Empty;
+
+        for (int i = fullName.Length - 1; i >= 0; i--)
+        {
+            fullNameReversed += fullName[i];
+        }
+
+        return fullNameReversed;
+    }
+
+}

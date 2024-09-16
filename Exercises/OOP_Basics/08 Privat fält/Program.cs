9fea7c5d546ad6f3e13f4dd8d36e79c8e9f7dd6e
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-16 19:13:56 +0200

    Add SetHeight().

--- Exercises/OOP_Basics/08 Privat fält/Program.cs
+++ Exercises/OOP_Basics/08 Privat fält/Program.cs
@@ -4,13 +4,64 @@
 // Lägg därför till en metod void SetLength(double length) som sätter värdet på det privat fältet length, 
 // samt en double GetLength() som returnerar värdet på fältet length.
 
-namespace _08_Privat_fält
+Person kalle = new Person() { FirstName = "Kalle", LastName = "Anka" };
+Person kajsa = new Person() { LastName = "Kajsa", FirstName = "Anka" };
+
+kalle.Mother = new Person() { FirstName = "Lisa", LastName = "Anka" };
+
+Console.WriteLine(kalle.GetSelfAndParents());
+
+
+
+
+
+
+class Person
 {
-    internal class Program
+    public string FirstName = string.Empty;
+    public string LastName = string.Empty;
+    
+    private double _Height = 0.0D;
+
+    public Person Mother;
+    public Person Father;
+
+    public string GetFullName()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        return $"{this.FirstName} {this.LastName}";
     }
+
+    public string GetFullName(string titel)
+    {
+        return $"{titel} {this.FirstName} {this.LastName}";
+    }
+
+    public string ReturnFullNameReversed()
+    {
+        string fullName = GetFullName();
+        string fullNameReversed = string.Empty;
+
+        for (int i = fullName.Length - 1; i >= 0; i--)
+        {
+            fullNameReversed += fullName[i];
+        }
+
+        return fullNameReversed;
+    }
+
+    public string GetSelfAndParents()
+    {
+        if (this.Mother == null)
+            this.Mother = new Person() { FirstName = "Okänd" };
+        if (this.Father == null)
+            this.Father = new Person() { FirstName = "Okänd" };
+
+        return $"{this.GetFullName()} - Mor: {this.Mother.GetFullName()} - Far: {this.Father.GetFullName()}";
+    }
+
+    public void SetHeight(double height)
+    {
+        _Height = height;
+    }
+
 }

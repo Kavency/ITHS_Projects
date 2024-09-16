63cd5ec467ec712f9b32eb90fe041c4291262497
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-16 13:36:44 +0200

    Completed exercise 02.

---
+++ Exercises/OOP_Basics/02 Metod returnerar namnet/Program.cs
@@ -0,0 +1,18 @@
+﻿Person kalle = new Person() { FirstName = "Kalle", LastName = "Anka" };
+Person kajsa = new Person() { LastName = "Kajsa", FirstName = "Anka" };
+
+Console.WriteLine(kalle.GetFullName());
+Console.WriteLine(kajsa.GetFullName());
+
+
+
+class Person
+{
+    public string FirstName;
+    public string LastName;
+
+    public string GetFullName()
+    {
+        return     $"{this.FirstName} {this.LastName}";
+    }
+}

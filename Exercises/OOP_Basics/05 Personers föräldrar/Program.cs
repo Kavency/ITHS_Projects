622cffd5a3df8099825667325682ba0351b211e0
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-16 15:01:45 +0200

    Update commit.

--- Exercises/OOP_Basics/05 Personers föräldrar/Program.cs
+++ Exercises/OOP_Basics/05 Personers föräldrar/Program.cs
@@ -3,11 +3,6 @@
 // Dessa representeras som publika fält(fields) av typen Person (eller vad du
 // kallade klassen i uppgift 1).
 
-// 4.Overload av GetFullName med titel.
-// GetFullName ska finnas i två versioner: en utan parametrar som i uppgift 2; 
-// och en där man kan skicka in en titel, t.ex. "Dr." eller "Mr." 
-// som returnerar namnet på personen med titeln framför.
-
 Person kalle = new Person() { FirstName = "Kalle", LastName = "Anka" };
 Person kajsa = new Person() { LastName = "Kajsa", FirstName = "Anka" };
 

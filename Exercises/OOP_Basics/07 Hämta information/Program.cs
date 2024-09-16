4238b8fc32ff1c73b7fa918c08311da2659f7076
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-16 15:10:41 +0200

    Add GetSelfAndParents().

--- Exercises/OOP_Basics/07 Hämta information/Program.cs
+++ Exercises/OOP_Basics/07 Hämta information/Program.cs
@@ -4,3 +4,51 @@
 // Om mammans eller pappans namn saknas (null) markera det med "okänd" i 
 // strängen. Exempel: "Fredrik Johansson - Mamma: okänd - Pappa: Göran Johansson"
 
+Person kalle = new Person() { FirstName = "Kalle", LastName = "Anka" };
+Person kajsa = new Person() { LastName = "Kajsa", FirstName = "Anka" };
+
+kalle.Mother = new Person() { FirstName = "Lisa", LastName = "Anka" };
+kalle.Father = new Person() { FirstName = "Jonas", LastName = "Anka" };
+
+Console.WriteLine(kalle.GetSelfAndParents());
+
+class Person
+{
+    public string FirstName;
+    public string LastName;
+
+    public Person Mother;
+    public Person Father;
+
+    public string GetFullName()
+    {
+        return $"{this.FirstName} {this.LastName}";
+    }
+
+    public string GetFullName(string titel)
+    {
+        return $"{titel} {this.FirstName} {this.LastName}";
+    }
+
+    public string ReturnFullNameReversed()
+    {
+        string fullName = GetFullName();
+        string fullNameReversed = string.Empty;
+
+        for (int i = fullName.Length - 1; i >= 0; i--)
+        {
+            fullNameReversed += fullName[i];
+        }
+
+        return fullNameReversed;
+    }
+
+    public string GetSelfAndParents()
+    {
+        Console.WriteLine(GetFullName());
+        Console.WriteLine(this.Mother.GetFullName());
+        Console.WriteLine(this.Father.GetFullName());
+        return "";
+    }
+
+}

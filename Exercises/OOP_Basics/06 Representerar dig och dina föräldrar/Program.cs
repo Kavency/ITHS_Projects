7e46e907924e77f197824de4b89245d810de6832
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-16 15:01:07 +0200

    Update comments.

--- Exercises/OOP_Basics/06 Representerar dig och dina föräldrar/Program.cs
+++ Exercises/OOP_Basics/06 Representerar dig och dina föräldrar/Program.cs
@@ -4,16 +4,6 @@
 // GetFullName() returnerar ditt namn, och myself.mother.GetFullName() 
 // returnerar din mammas namn.
 
-// 5.Lägg till så personer kan ha föräldrar.
-// Uppdatera person-klassen så att varje person kan ha en mamma och en pappa.
-// Dessa representeras som publika fält(fields) av typen Person (eller vad du
-// kallade klassen i uppgift 1).
-
-// 4.Overload av GetFullName med titel.
-// GetFullName ska finnas i två versioner: en utan parametrar som i uppgift 2; 
-// och en där man kan skicka in en titel, t.ex. "Dr." eller "Mr." 
-// som returnerar namnet på personen med titeln framför.
-
 Person kalle = new Person() { FirstName = "Kalle", LastName = "Anka" };
 Person kajsa = new Person() { LastName = "Kajsa", FirstName = "Anka" };
 

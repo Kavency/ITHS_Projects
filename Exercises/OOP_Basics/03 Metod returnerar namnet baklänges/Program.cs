57a89a11946645b1696396e81c035b0655712f3f
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-16 13:45:18 +0200

    Add methods.

--- Exercises/OOP_Basics/03 Metod returnerar namnet baklänges/Program.cs
+++ Exercises/OOP_Basics/03 Metod returnerar namnet baklänges/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 3.Lägg till en metod som returnerar namnet baklänges
+// Uppdatera klassen med en ny metod: public string GetFullNameReversed() som 
+// returnerar en sträng med hela namnet baklänges. Låt denna metoden använda
+// sig av metoden i uppgift 2.
+
+
+class Person
+{
+    public string FirstName;
+    public string LastName;
+
+    public string GetFullName()
+    {
+        return $"{this.FirstName} {this.LastName}";
+    }
+
+    public string ReturnFullNameReversed()
+    {
+        return 
+    }
+
+}

2a955ed07a7d09175be103b6f5a8f4f4689b8fa8
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-16 19:09:21 +0200

    First commit.

---
+++ Exercises/OOP_Basics/08 Privat fält/Program.cs
@@ -0,0 +1,16 @@
+﻿// 8.Privat fält, med metoder för att sätta och hämta värdet.
+// Uppdatera klassen med ett fält, private double length, som kan lagra längden på personen.
+// Eftersom denna är markerad som private kommer man inte kunna läsa/skriva denna utanför klassen.
+// Lägg därför till en metod void SetLength(double length) som sätter värdet på det privat fältet length, 
+// samt en double GetLength() som returnerar värdet på fältet length.
+
+namespace _08_Privat_fält
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

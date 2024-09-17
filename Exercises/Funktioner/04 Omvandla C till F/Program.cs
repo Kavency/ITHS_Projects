16eb94623e7f9d27edb79bff8985e5f056278e7b
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-17 18:54:05 +0200

    First commit.

---
+++ Exercises/Funktioner/04 Omvandla C till F/Program.cs
@@ -0,0 +1,4 @@
+﻿// 4.Omvandla Celsius till Fahrenheit
+// Skriv en funktion som översätter ett värde från Celsius till Fahrenheit.
+// Både in-parameter och returvärde ska vara av datatyp double.
+

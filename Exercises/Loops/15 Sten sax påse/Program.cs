1b1617726a238fa1360ae1f9c269b7f4b1a6bf61
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-17 12:14:23 +0200

    Completed exercise 15.

--- Exercises/Loops/15 Sten sax påse/Program.cs
+++ Exercises/Loops/15 Sten sax påse/Program.cs
@@ -4,4 +4,72 @@
 // Lägg det hela i en loop så spelet fortsätter tills man matar in en tom sträng (trycker enter utan att skriva något). 
 // Skriv även ut poäng.
 
+Random slumpGenerator = new Random();
+
+string userChoice = string.Empty;
+string cpuChoice = string.Empty;
+int userScore = 0;
+int cpuScore = 0;
+int counter = 0;
+int randomNum = slumpGenerator.Next(1, 4);
+
+while (counter < 5)
+{
+    Console.Clear();
+    Console.WriteLine("Välj (1)Sten (2)Sax (3)Påse: ");
+    userChoice = Console.ReadLine();
+    if (userChoice == "")
+        break;
+
+    userChoice = AssignChoices(Int32.Parse(userChoice));
+    cpuChoice = AssignChoices(slumpGenerator.Next(1, 4));
+
+    Console.WriteLine(userChoice);
+    Console.WriteLine(cpuChoice);
+    
+    if (userChoice == cpuChoice)
+    {
+        Console.WriteLine("oavgjort");
+    }
+    else if (userChoice == "Sten" && cpuChoice == "Sax"
+        || userChoice == "Sax" && cpuChoice == "Påse"
+        || userChoice == "Påse" && cpuChoice == "Sten")
+    {
+        Console.WriteLine("Player wins.");
+        userScore++;
+    }
+    else
+    {
+        Console.WriteLine("CPU wins");
+        cpuScore++;
+    }
+    counter++;
+    Thread.Sleep(1000);
+}
+
+Console.Clear();
+if(cpuScore > userScore)
+    Console.WriteLine("You are a big looooser!");
+else
+    Console.WriteLine("Peew... some lucky one!");
+Console.WriteLine();
+Console.WriteLine("Scores:");
+Console.WriteLine($"Player: {userScore}");
+Console.WriteLine($"CPU: {cpuScore}");
+
+
+string AssignChoices(int number)
+{
+    switch (number)
+    {
+        case 1:
+            return "Sten";
+        case 2:
+            return "Sax";
+        case 3:
+            return "Påse";
+        default:
+            return "";
+    }
+}
 

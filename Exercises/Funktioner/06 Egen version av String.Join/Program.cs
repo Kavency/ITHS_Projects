af3d35f8a712a419d260eed3a7676ef007c15d33
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-17 18:37:20 +0200

    Completed exercise 06.

---
+++ Exercises/Funktioner/06 Egen version av String.Join/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine(MyJoin("->", "Sverige", "Norge", "Danmark", "Finland", "Island"));
+
+string MyJoin(string separator, params string[] strings)
+{
+	string newString = string.Empty;
+	bool isFirstEntry = true;
+
+	foreach (var item in strings)
+	{
+		if (isFirstEntry)
+		{
+			newString += item;
+			isFirstEntry = false;
+		}
+		else
+			newString += separator + item;
+	}
+
+	return newString;
+}

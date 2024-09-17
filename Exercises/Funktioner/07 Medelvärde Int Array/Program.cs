22e5e97f67c17c3e602a85dac28e1bec80fc25cb
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-17 19:52:41 +0200

    Completed exercise 07.

--- Exercises/Funktioner/07 Medelvärde Int Array/Program.cs
+++ Exercises/Funktioner/07 Medelvärde Int Array/Program.cs
@@ -2,3 +2,18 @@
 // Skriv en funktion som tar en int[] in, 
 // och returnerar medelvärdet som en double.
 
+int[] numbers = new int[] { 10, 4, 6, 3, 9, 12, 6, 3, 9, 5 };
+
+Console.WriteLine(CalculateAverage(numbers));
+
+double CalculateAverage(int[] values)
+{
+    double total = 0;
+
+    foreach(int value in values)
+    {
+        total += value;
+    }
+
+    return total / values.Length;
+}

d3f8cd02b633545712350d9fe00ba24f6c3fe3a6
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-17 20:22:11 +0200

    Completed exercise 08.

--- Exercises/Funktioner/08 Siffror till text/Program.cs
+++ Exercises/Funktioner/08 Siffror till text/Program.cs
@@ -2,3 +2,44 @@
 // Skriv en funktion som tar ett heltal in, och returnerar en string[] där 
 // varje element innehåller ordet för varje siffra i talet.
 
+Console.Write("Enter a number: ");
+int number = Int32.Parse(Console.ReadLine());
+
+foreach (string item in NumbersToText(number))
+{
+    Console.Write(item + " ");
+}
+
+string[] NumbersToText(int number)
+{
+    string numberAsString = number.ToString();
+    string[] returnArray = new string[numberAsString.Length];
+
+	for(int i = 0; i < numberAsString.Length; i++)
+    {
+
+        if (numberAsString[i] == '0')
+            returnArray[i] = "Zero";
+        else if(numberAsString[i] == '1')
+            returnArray[i] = "One";
+        else if (numberAsString[i] == '2')
+            returnArray[i] = "Two";
+        else if (numberAsString[i] == '3')
+            returnArray[i] = "Three";
+        else if (numberAsString[i] == '4')
+            returnArray[i] = "Four";
+        else if (numberAsString[i] == '5')
+            returnArray[i] = "Five";
+        else if (numberAsString[i] == '6')
+            returnArray[i] = "Six";
+        else if (numberAsString[i] == '7')
+            returnArray[i] = "Seven";
+        else if (numberAsString[i] == '8')
+            returnArray[i] = "Eight";
+        else if (numberAsString[i] == '9')
+            returnArray[i] = "Nine";
+    }
+
+    return returnArray;
+    
+}

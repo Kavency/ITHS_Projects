5da4497e15a0637e4ab64c2b6b4c364fb502b213
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-17 08:07:10 +0200

    Completed exercise 05.

--- Exercises/Indexing/05 Palindrom/Program.cs
+++ Exercises/Indexing/05 Palindrom/Program.cs
@@ -2,4 +2,16 @@
 // Ett palindrom är ett ord som blir samma framlänges som baklänges.
 // Be användaren skriva in ett ord och ange sedan om det är ett palindrom eller inte.
 
+Console.Write("Enter a word: ");
+string userInput = Console.ReadLine();
+string palindrom = string.Empty;
 
+for(int i = userInput.Length - 1; i >= 0; i--)
+{
+    palindrom += userInput[i];
+}
+
+if(userInput.ToLower() == palindrom.ToLower())
+    Console.WriteLine($"{userInput} är ett palindrom.");
+else
+    Console.WriteLine($"{userInput} är inte ett palindrom.");

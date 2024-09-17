48aa23c27d3f61b80ddde15c66964479380fbc15
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-17 11:07:15 +0200

    First commit.

---
+++ Exercises/Loops/15 Sten sax påse/Program.cs
@@ -0,0 +1,7 @@
+﻿// 15.Sten, sax, påse
+// Skriv ett sten-sax-påse spel. Användaren skriver in “sten”, “sax” eller “påse”.
+// Dators val slumpas med Random. Skriv sedan ut vad datorn valde, och vem som vann omgången.
+// Lägg det hela i en loop så spelet fortsätter tills man matar in en tom sträng (trycker enter utan att skriva något). 
+// Skriv även ut poäng.
+
+

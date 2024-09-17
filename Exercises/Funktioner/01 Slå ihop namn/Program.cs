a1e31e394e13a65a85a8549a60c43234aceda32f
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-17 12:35:22 +0200

    Completed exercise 01.

--- Exercises/Funktioner/01 Slå ihop namn/Program.cs
+++ Exercises/Funktioner/01 Slå ihop namn/Program.cs
@@ -3,3 +3,16 @@
 // Funktionen ska skriva ut hela namnet på skärmen. 
 // Testa genom att anropa funktionen med ditt namn.
 
+Console.Write("Enter your first name: ");
+string firstName = Console.ReadLine();
+Console.Write("Enter your last name: ");
+string lastName = Console.ReadLine();
+
+
+CombineNames(firstName, lastName);
+
+
+void CombineNames(string firstName, string lastName)
+{
+    Console.WriteLine($"{firstName} {lastName}");
+}

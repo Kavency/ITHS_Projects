3cd5ab18045bd76bc915cc3b1ced6280210b93fc
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-17 19:10:57 +0200

    Completed exercise 05.

--- Exercises/Funktioner/05 Bindesträck i sträng/Program.cs
+++ Exercises/Funktioner/05 Bindesträck i sträng/Program.cs
@@ -3,4 +3,28 @@
 // bindestreck mellan varje tecken. T.ex om man skickar in “Fredrik” , 
 // så returnerar den “F-r-e-d-r-i-k”
 
+Console.Write("Enter a string: ");
+string userString = Console.ReadLine();
 
+Console.WriteLine(HiphonsInString(userString));
+
+
+string HiphonsInString(string value)
+{
+    string returnString = string.Empty;
+    int length = 1;
+
+    foreach(char character in value)
+    {
+        if(length == value.Length)
+        {
+            returnString += character;
+            break;
+        }
+        
+        returnString += character + "-";
+        length++;
+    }
+
+    return returnString;
+}

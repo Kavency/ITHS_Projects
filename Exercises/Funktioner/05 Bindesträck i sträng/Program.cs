fd844b06d1cf6e34a0da2fe243fe536c202c8ec0
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-17 19:03:12 +0200

    First commit.

---
+++ Exercises/Funktioner/05 Bindesträck i sträng/Program.cs
@@ -0,0 +1,6 @@
+﻿// 5.Lägg in bindesträck mellan tecken i en sträng
+// Skriv en funktion som tar in en sträng och returnerar en sträng med ett
+// bindestreck mellan varje tecken. T.ex om man skickar in “Fredrik” , 
+// så returnerar den “F-r-e-d-r-i-k”
+
+

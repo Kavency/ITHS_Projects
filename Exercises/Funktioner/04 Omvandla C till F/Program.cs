97d4267d4b361ae807f1dbfd9ed54661738b78b1
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-17 19:01:21 +0200

    Completed exercise 04.

--- Exercises/Funktioner/04 Omvandla C till F/Program.cs
+++ Exercises/Funktioner/04 Omvandla C till F/Program.cs
@@ -2,3 +2,9 @@
 // Skriv en funktion som översätter ett värde från Celsius till Fahrenheit.
 // Både in-parameter och returvärde ska vara av datatyp double.
 
+Console.WriteLine(ConvertCelsiusToFahrenheit(25));
+
+double ConvertCelsiusToFahrenheit(double degree)
+{
+    return degree * 9/5 + 32;
+}

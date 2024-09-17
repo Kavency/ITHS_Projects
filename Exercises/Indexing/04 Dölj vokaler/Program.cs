c182307128a332f909361fa30de59451d880d6eb
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-17 07:57:54 +0200

    Completed exercise 04.

--- Exercises/Indexing/04 Dölj vokaler/Program.cs
+++ Exercises/Indexing/04 Dölj vokaler/Program.cs
@@ -1,8 +1,7 @@
 ﻿// 4.Dölj vokaler
 // Be användaren mata in en text. Skriv ut texten med alla vokaler ersatta med *
 
-char[] vokaler = { 'a', 'e', 'i', 'o', 'u', 'y', 'å', 'ä', 'ö' };
-string vokalerString = "aeiouyåäö"; // Enklaste lösningen är med en sträng.
+string vowelString = "aeiouyåäö";
 
 Console.Write("Enter any text: ");
 string userInput = Console.ReadLine();
@@ -11,18 +10,8 @@ Console.WriteLine();
 
 for (int i = 0; i < userInput.Length; i++)
 {
-    if (vokalerString.Contains(userInput[i])) // Ingen kontroll av versaler sker.
+    if (vowelString.Contains(userInput[i]))
         Console.Write('*');
     else
         Console.Write(userInput[i]);
-}
-
-for (int i = 0; i < userInput.Length; i++)
-{
-    bool isVokal = false; // Minns inte vad vokal heter på engelska.
-    foreach(var vokal in vokaler)
-    {
-        if(vokal == userInput[i])
-            isVokal = true;
-    }
 }

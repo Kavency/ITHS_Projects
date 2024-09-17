ebbdfc420546fe2092b0a33474a160c96ead8439
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-17 20:45:41 +0200

    First commit.

---
+++ Exercises/Funktioner/10 Hitta index/Program.cs
@@ -0,0 +1,4 @@
+﻿// 10.Hitta index för alla förekomster av ett givet tecken.
+// Skapa funktionen int[] IndexesOf(string text, char c) som söker igenom strängen 
+// text och returnerar en int[] med index till alla förekomster av c i text.
+

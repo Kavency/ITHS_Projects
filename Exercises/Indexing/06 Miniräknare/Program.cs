d7bc3affc8f98790506d7d480279a7fcbad828a8
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-17 12:28:07 +0200

    Add user input.

--- Exercises/Indexing/06 Miniräknare/Program.cs
+++ Exercises/Indexing/06 Miniräknare/Program.cs
@@ -4,3 +4,6 @@
 // nu ske på en rad (första talet, operator, andra talet). Ignorera inmatade mellanslag
 // i strängen. Om man t.ex. matar in strängen "34 - 14", så ska programmet skriva ut "= 20".
 
+Console.WriteLine("Vad vill du räkna ut? ");
+string userInput = Console.ReadLine();
+

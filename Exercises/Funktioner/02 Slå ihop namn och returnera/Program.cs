f46a5ec439187bf24367e2f78bbbc22d1bfbbc1b
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-17 18:40:13 +0200

    Completed exercise 02.

---
+++ Exercises/Funktioner/02 Slå ihop namn och returnera/Program.cs
@@ -0,0 +1,17 @@
+﻿// 2.Slå ihop för- och efternamn - returnera
+// Skriv om funktionen ovan så att den istället för att skriva ut namnet returnerar 
+// en string med hela namnet. Anropa funktionen och skriv ut returvärdet.
+
+Console.Write("Enter your first name: ");
+string firstName = Console.ReadLine();
+Console.Write("Enter your last name: ");
+string lastName = Console.ReadLine();
+
+
+Console.WriteLine(CombineNames(firstName, lastName));
+
+
+string CombineNames(string firstName, string lastName)
+{
+    return $"{firstName} {lastName}";
+}

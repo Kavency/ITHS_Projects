a86acfd87b2b0fec1a72aba3e9bf9a49458c65f4
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-17 12:31:21 +0200

    First commit.

---
+++ Exercises/Funktioner/01 Slå ihop namn/Program.cs
@@ -0,0 +1,5 @@
+﻿// 1.Slå ihop för- och efternamn - skriv ut
+// Skriv en funktion som tar två parametrar: firstName och lastName. 
+// Funktionen ska skriva ut hela namnet på skärmen. 
+// Testa genom att anropa funktionen med ditt namn.
+

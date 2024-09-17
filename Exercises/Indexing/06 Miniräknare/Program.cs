0ea886eb1dfed39b1d63cf7b62787d2bfdedcceb
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-17 08:09:30 +0200

    First commit.

---
+++ Exercises/Indexing/06 Miniräknare/Program.cs
@@ -0,0 +1,6 @@
+﻿// 06 Miniräknare
+// Skapa en enkel miniräknare som kan hantera de fyra räknesätten med två operander.
+// Till skillnad från uppgift 6 i övningsuppgifter med variabler, så ska inmatningen
+// nu ske på en rad (första talet, operator, andra talet). Ignorera inmatade mellanslag
+// i strängen. Om man t.ex. matar in strängen "34 - 14", så ska programmet skriva ut "= 20".
+

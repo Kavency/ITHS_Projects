4b200c984c84e19d1d656c4bad3352a05f1f2200
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-17 20:40:51 +0200

    Complete exercise 09.

--- Exercises/Funktioner/09 Heltal till text/Program.cs
+++ Exercises/Funktioner/09 Heltal till text/Program.cs
@@ -2,4 +2,44 @@
 // Skriv en funktion som tar en ushort som parameter, 
 // och returnerar en sträng med numret utskrivet i ord.
 
+Console.Write("Enter a number: ");
+string number = Console.ReadLine();
 
+foreach (string item in IntegerToText(ushort.Parse(number)))
+{
+    Console.Write(item + " ");
+}
+
+string[] IntegerToText(ushort number)
+{
+    string numberAsString = number.ToString();
+    string[] returnArray = new string[numberAsString.Length];
+
+    for (int i = 0; i < numberAsString.Length; i++)
+    {
+
+        if (numberAsString[i] == '0')
+            returnArray[i] = "Zero";
+        else if (numberAsString[i] == '1')
+            returnArray[i] = "One";
+        else if (numberAsString[i] == '2')
+            returnArray[i] = "Two";
+        else if (numberAsString[i] == '3')
+            returnArray[i] = "Three";
+        else if (numberAsString[i] == '4')
+            returnArray[i] = "Four";
+        else if (numberAsString[i] == '5')
+            returnArray[i] = "Five";
+        else if (numberAsString[i] == '6')
+            returnArray[i] = "Six";
+        else if (numberAsString[i] == '7')
+            returnArray[i] = "Seven";
+        else if (numberAsString[i] == '8')
+            returnArray[i] = "Eight";
+        else if (numberAsString[i] == '9')
+            returnArray[i] = "Nine";
+    }
+
+    return returnArray;
+
+}

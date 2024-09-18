4ed84a2a4bcd036150c7e06d4fbfc168db10bd09
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-18 11:25:21 +0200

    First commit.

---
+++ Exercises/Indexing/11 Start Stop Index/Program.cs
@@ -0,0 +1,5 @@
+﻿// 11.Start & Stop
+// Be användaren mata in en text. Be sedan om ett startindex, 
+// och sedan om ett stopindex. Skriv ut hela texten, 
+// men alla tecken från startindex till stopindex ska vara röda.
+

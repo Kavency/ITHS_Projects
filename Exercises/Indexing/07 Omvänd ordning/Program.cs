5497850caf2fcde7daa9b370a857457657b8011f
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-18 09:14:02 +0200

    First commit.

---
+++ Exercises/Indexing/07 Omvänd ordning/Program.cs
@@ -0,0 +1,6 @@
+﻿// 7.Omvänd ordning
+// Be användaren skriva in ett ord, sedan ett till, och ett till; 
+// tills man skrivit in 7 ord. Skriv sedan ut orden på skärmen i 
+// bakvänd ordning mot hur de matades in. Alltså ordet man skrev 
+// in sist, skrivs ut först.
+

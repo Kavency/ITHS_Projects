b903deb0f8d058a708210537e23d6ab36d787f91
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-18 11:23:23 +0200

    Complete exercise 10.

--- Exercises/Indexing/10 Färgade bokstäver/Program.cs
+++ Exercises/Indexing/10 Färgade bokstäver/Program.cs
@@ -1,7 +1,23 @@
 ﻿// 10.Färgade bokstäver
-// Man kan byta färg som används för utskrift i console med hjälp av Console.
-// ForegroundColor (t.ex Console.ForegroundColor = ConsoleColor.Red)
+// Man kan byta färg som används för utskrift i console med hjälp av
+// Console.ForegroundColor (t.ex Console.ForegroundColor = ConsoleColor.Red)
 // Skapa ett program som ber användaren mata in en text. Be sedan användaren mata in en bokstav.
 // Skriv ut strängen som matades in; de bokstäver som är samma som den användaren valde ska
 // skriva ut med röd färg, alla andra med grå..
 
+Console.Write("Enter a word: ");
+string userInput = Console.ReadLine();
+Console.Write($"Enter a letter in {userInput}: ");
+char userChar = char.Parse(Console.ReadLine());
+
+for (int i = 0; i < userInput.Length; i++)
+{
+	if (userInput[i] == userChar)
+	{
+		Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write(userInput[i]);
+        Console.ForegroundColor = ConsoleColor.Gray;
+	}
+	else
+		Console.Write(userInput[i]);
+}

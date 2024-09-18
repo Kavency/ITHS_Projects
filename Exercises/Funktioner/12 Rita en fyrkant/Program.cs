f80d810394cb4ddc32b757a6e920f8dba43be2d2
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-18 16:04:30 +0200

    First commit.

---
+++ Exercises/Funktioner/12 Rita en fyrkant/Program.cs
@@ -0,0 +1,5 @@
+﻿// 12.Rita en box
+// Skriv en funktion DrawBox(int width, int height). När man anropar 
+// funktionen ska den skriva ut en rektangel där de yttre tecknen 
+// består av ‘#’ och de inre av ‘-’.
+

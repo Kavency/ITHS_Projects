38f743729a90289fede70aff65f56fcabf5f7472
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-18 17:23:43 +0200

    Completed exercise 12.

--- Exercises/Funktioner/12 Rita en fyrkant/Program.cs
+++ Exercises/Funktioner/12 Rita en fyrkant/Program.cs
@@ -3,3 +3,24 @@
 // funktionen ska den skriva ut en rektangel där de yttre tecknen 
 // består av ‘#’ och de inre av ‘-’.
 
+DrawBox(25, 25);
+
+
+void DrawBox(int width, int height)
+{
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			if(i == 0 || i == height - 1 || j == 0 || j == width - 1)
+			{
+				Console.Write("*");
+			}
+			else
+			{
+				Console.Write("-");
+			}
+		}
+        Console.WriteLine();
+	}
+}

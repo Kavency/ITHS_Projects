026d9545723c40f44a9d9a797a9afa3c734da4f4
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-18 14:27:32 +0200

    First commit.

---
+++ Exercises/Indexing/12 Växla färg/Program.cs
@@ -0,0 +1,5 @@
+﻿// 12.Växla färg
+// Be användaren mata in en text. Be sedan användaren mata in en bokstav. 
+// Skriv ut hela texten, rödmarkera från första stället den hittar bokstaven 
+// till andra stället den hittar bokstaven, från tredje stället till fjärde o.s.v.
+

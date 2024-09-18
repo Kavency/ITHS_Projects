e8e15e10def30ef21a5a37062b16b40a7fc6e3d1
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-18 08:50:59 +0200

    Complete exercise 06.

--- Exercises/Indexing/06 Miniräknare/Program.cs
+++ Exercises/Indexing/06 Miniräknare/Program.cs
@@ -4,6 +4,55 @@
 // nu ske på en rad (första talet, operator, andra talet). Ignorera inmatade mellanslag
 // i strängen. Om man t.ex. matar in strängen "34 - 14", så ska programmet skriva ut "= 20".
 
-Console.WriteLine("Vad vill du räkna ut? ");
-string userInput = Console.ReadLine();
+string userInput = string.Empty;
+string num1Str, num2Str;
+double num1, num2;
+char opChar = '+';
+double result = 0;
 
+while (true)
+{
+    Console.Clear();
+    Console.Write("Vad vill du räkna ut? ");
+    userInput = Console.ReadLine();
+
+    opChar = GetOperator(userInput);
+
+    if (userInput.ToLower() == string.Empty || userInput == "x")
+        break;
+    
+    num1Str = userInput.Substring(0, userInput.IndexOf(opChar));
+    num2Str = userInput.Substring(userInput.IndexOf(opChar) + 1);
+    num1Str = num1Str.Trim();
+    num2Str = num2Str.Trim();
+
+    num1 = double.Parse(num1Str);
+    num2 = double.Parse(num2Str);
+
+    if (opChar == '+')
+        result = num1 + num2;
+    else if (opChar == '-')
+        result = num1 - num2;
+    else if (opChar == '/')
+        result = num1 / num2;
+    else if (opChar == '*')
+        result = num1 * num2;
+    
+    Console.WriteLine($"{num1Str} {opChar} {num2Str} = {result}");
+    Console.WriteLine("Press anykey to continue.");
+    Console.ReadKey();
+}
+
+char GetOperator(string value)
+{
+    if (value.Contains('+'))
+        return '+';
+    else if (value.Contains('-'))
+        return '-';
+    else if (value.Contains('/'))
+        return '/';
+    else if (value.Contains('*'))
+        return '*';
+    else
+       return ' ' ;
+}

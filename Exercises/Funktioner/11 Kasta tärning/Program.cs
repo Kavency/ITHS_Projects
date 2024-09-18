0f812c61006314f70452b9a1cf4a3e664502c5b9
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-18 15:53:28 +0200

    First commit.

---
+++ Exercises/Funktioner/11 Kasta tärning/Program.cs
@@ -0,0 +1,8 @@
+﻿// 11.Kasta tärning
+// Skriv en funktion ThrowDice() som returnerar ett slumpmässigt heltal 1-6. 
+// Skriv en annan funktion ThrowMultipleDice(int n) som returnerar resultatet 
+// av att man slagit n tärningar.
+
+// Extra utmaning: Lägg till en frivillig(optional) int på båda funktionerna, 
+// där man kan ange antal sidor på tärningen (default 6).
+

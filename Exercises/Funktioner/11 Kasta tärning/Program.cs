8855243ec0e2b95bac261c2d108d27172c04f9ba
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-18 16:02:36 +0200

    Completed exercise 11.

--- Exercises/Funktioner/11 Kasta tärning/Program.cs
+++ Exercises/Funktioner/11 Kasta tärning/Program.cs
@@ -6,3 +6,22 @@
 // Extra utmaning: Lägg till en frivillig(optional) int på båda funktionerna, 
 // där man kan ange antal sidor på tärningen (default 6).
 
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine($"One die: {ThrowDice()}");
+    Console.WriteLine($"Two dice: {ThrowMultiDice()}");
+    Console.WriteLine("Press any key to roll again.");
+    Console.ReadLine();
+}
+
+int ThrowDice(int noOfSides = 6)
+{
+    Random rnd = new();
+    return rnd.Next(1, noOfSides + 1);
+}
+int ThrowMultiDice(int noOfSides = 6)
+{
+    Random rnd = new();
+    return rnd.Next(1, noOfSides + 1) + rnd.Next(1, noOfSides + 1);
+}

6a932e5477d4536aa3ce1c84cf06884ee6a964bd
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-18 10:17:51 +0200

    First commit.

---
+++ Exercises/Indexing/10 Färgade bokstäver/Program.cs
@@ -0,0 +1,7 @@
+﻿// 10.Färgade bokstäver
+// Man kan byta färg som används för utskrift i console med hjälp av Console.
+// ForegroundColor (t.ex Console.ForegroundColor = ConsoleColor.Red)
+// Skapa ett program som ber användaren mata in en text. Be sedan användaren mata in en bokstav.
+// Skriv ut strängen som matades in; de bokstäver som är samma som den användaren valde ska
+// skriva ut med röd färg, alla andra med grå..
+

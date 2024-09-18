501aa1c05785f9698a7c8b2631c7b0ecbab32a74
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-18 15:51:07 +0200

    Completed exercise 10.

--- Exercises/Funktioner/10 Hitta index/Program.cs
+++ Exercises/Funktioner/10 Hitta index/Program.cs
@@ -2,10 +2,36 @@
 // Skapa funktionen int[] IndexesOf(string text, char c) som söker igenom strängen 
 // text och returnerar en int[] med index till alla förekomster av c i text.
 
-Console.Write("Enter a string: ");
-string userString = Console.ReadLine();
+int[] newArray = IndexOf("assa brassa mandelmassa", 'a'); ;
+
+foreach (var item in newArray)
+{
+    Console.WriteLine(item);
+}
 
 int[] IndexOf(string stringToCheck, char charToCheckFor)
 {
-    
+    int numberOfOccurences = 0;
+    int arrayPosition = 0;
+
+    for (int i = 0; i < stringToCheck.Length; i++)
+    {
+        if (stringToCheck[i] == charToCheckFor)
+        {
+            numberOfOccurences++;
+        }
+    }
+
+    int[] returnArray = new int[numberOfOccurences];
+
+    for (int i = 0; i < stringToCheck.Length; i++)
+    {
+        if (stringToCheck[i] == charToCheckFor)
+        {
+            returnArray[arrayPosition] = i;
+            arrayPosition++;
+        }
+    }
+
+    return returnArray;
 }

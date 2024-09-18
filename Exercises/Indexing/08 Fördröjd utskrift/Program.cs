bc9d41ec9877c6a1b6e538ed7b8a2099774193e7
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-18 09:56:01 +0200

    First commit.

---
+++ Exercises/Indexing/08 Fördröjd utskrift/Program.cs
@@ -0,0 +1,7 @@
+﻿// 8.Fördröjd utskrift
+// Skapa ett program som ber användaren att skriva in ett ord. Sedan ett till, 
+// och ett till i all evighet. Varje gång man skrivit in ett ord så ska 
+// programmet skriva ut det ord man skrev 10 inmatningar tidigare. Men om man 
+// inte skrivit in 10 ord än så kan den istället skriva just det: 
+// “Du har inte skrivit in 10 ord än.”
+

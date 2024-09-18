3f5527191320466b7a6405c076798264185b5da9
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-18 10:09:48 +0200

    Complete exercise 08.

--- Exercises/Indexing/08 Fördröjd utskrift/Program.cs
+++ Exercises/Indexing/08 Fördröjd utskrift/Program.cs
@@ -5,3 +5,17 @@
 // inte skrivit in 10 ord än så kan den istället skriva just det: 
 // “Du har inte skrivit in 10 ord än.”
 
+List<string> userWords = new List<string>();
+int i = 1;
+
+while (true)
+{
+    Console.Write($"Skriv in ord {i}: ");
+    userWords.Add(Console.ReadLine());
+
+    if(i >= 10)
+        Console.WriteLine(userWords[i - 10]);
+    else
+        Console.WriteLine("Skriv in minst 10 ord.");
+    i++;
+}

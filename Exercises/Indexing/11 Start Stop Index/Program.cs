b9e1b59b76c527eccbd0716057222a358933d3cc
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-18 14:25:55 +0200

    Completed exercise 11.

--- Exercises/Indexing/11 Start Stop Index/Program.cs
+++ Exercises/Indexing/11 Start Stop Index/Program.cs
@@ -3,3 +3,25 @@
 // och sedan om ett stopindex. Skriv ut hela texten, 
 // men alla tecken från startindex till stopindex ska vara röda.
 
+int startIndex = 0;
+int stopIndex = 0;
+
+Console.Write("Enter a word: ");
+string userInput = Console.ReadLine();
+
+Console.Write("Enter a start number: ");
+startIndex = Int32.Parse(Console.ReadLine());
+Console.Write("Enter a stop number: ");
+stopIndex = Int32.Parse(Console.ReadLine());
+
+for(int i = 0; i < userInput.Length; i++)
+{
+    if(userInput.Length - 1 > i && i >= startIndex - 1 && i <= stopIndex -1)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write(userInput[i]);
+        Console.ForegroundColor = ConsoleColor.Gray;
+    }
+    else
+        Console.Write(userInput[i]);
+}

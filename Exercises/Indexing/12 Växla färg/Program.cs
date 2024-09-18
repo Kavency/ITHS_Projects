66fd4b7ce2cca55663e7989e8ca8683f801800c5
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-18 15:19:04 +0200

    Completed exercise 12.

--- Exercises/Indexing/12 Växla färg/Program.cs
+++ Exercises/Indexing/12 Växla färg/Program.cs
@@ -3,3 +3,28 @@
 // Skriv ut hela texten, rödmarkera från första stället den hittar bokstaven 
 // till andra stället den hittar bokstaven, från tredje stället till fjärde o.s.v.
 
+Console.ForegroundColor = ConsoleColor.Gray;
+Console.Write("Enter a string: ");
+string userInput = Console.ReadLine();
+Console.Write($"Enter a letter: ");
+char userChar = char.Parse(Console.ReadLine());
+
+int isColoured = -1;
+
+for (int i = 0; i < userInput.Length; i++)
+{
+    if (userInput[i] == userChar)
+    {
+        isColoured *= -1;
+    }
+    if(isColoured > 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write(userInput[i]);
+    }
+    else
+    {
+        Console.Write(userInput[i]);
+        Console.ForegroundColor = ConsoleColor.Gray;
+    }
+}

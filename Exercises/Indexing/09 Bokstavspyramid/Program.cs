f50ca526900ef5aaf8ad7fae02eef30380d7079b
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-18 10:15:27 +0200

    Complete exercise 09.

--- Exercises/Indexing/09 Bokstavspyramid/Program.cs
+++ Exercises/Indexing/09 Bokstavspyramid/Program.cs
@@ -2,3 +2,13 @@
 // Skapa ett program som skriver ut första tecknet i "Hello World" på första raden, 
 // de 2 första tecknen på andra raden osv.
 
+string helloWorld = "Hello World!";
+
+for (int i = 0; i < helloWorld.Length; i++)
+{
+	for (int j = 0; j <= i; j++)
+	{
+		Console.Write(helloWorld[j]);
+	}
+    Console.WriteLine();
+}

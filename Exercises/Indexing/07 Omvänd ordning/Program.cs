ecd5fe7296009e91cb6bf88aeda2c7b72f5eed56
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-18 09:50:44 +0200

    Complete exercise 07.

--- Exercises/Indexing/07 Omvänd ordning/Program.cs
+++ Exercises/Indexing/07 Omvänd ordning/Program.cs
@@ -4,3 +4,15 @@
 // bakvänd ordning mot hur de matades in. Alltså ordet man skrev 
 // in sist, skrivs ut först.
 
+string[] userStrings = new string[7];
+
+for(int i = 0; i < 7; i++)
+{
+    Console.Write($"Skriv in ord {i + 1}: ");
+    userStrings[i] = Console.ReadLine();
+}
+
+for(int i = userStrings.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(userStrings[i]);
+}

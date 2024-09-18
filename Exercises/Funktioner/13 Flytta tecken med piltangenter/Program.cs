031fcb48320ed9836076d6d2aaa9538000af6202
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-18 17:26:37 +0200

    First commit.

---
+++ Exercises/Funktioner/13 Flytta tecken med piltangenter/Program.cs
@@ -0,0 +1,10 @@
+﻿// 13.Flytta runt ett @ med piltangenterna.
+// Använd DrawBox-funktionen i föregående uppgift för att rita en box på skärmen. 
+// Placera sedan ett @ i mitten av boxen. Om man använder piltangenterna ska man 
+// kunna flytta runt @. När den kommer till kanten av boxen så ska den inte kunna
+// gå längre åt det hållet.
+
+// Tips: För att flytta @ behöver du skriva ‘-’ på dess tidigare position 
+// och ‘@’ på den nya positionen. Spara bredd och höjd på boxen så du vet när den 
+// ska stanna.
+

66308bb95d62caadf0d14932657c192a34ceaea6
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-19 07:14:21 +0200

    First commit.

---
+++ Exercises/Properties/03 ReadOnly Prop FullName/Program.cs
@@ -0,0 +1,6 @@
+﻿// 3.Read - only property FullName
+// Lägg sedan till en publik property FullName som är read only (bara get;)
+// När man läser av propertyn så ska den returnera hela namnet. Testa koden
+// genom att skapa ett objekt, tilldela värden på både FirstName och LastName, 
+// och sedan skriva ut FullName.
+

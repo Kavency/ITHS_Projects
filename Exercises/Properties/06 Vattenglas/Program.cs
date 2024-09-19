ba07d95d4854df3dab5342a219d8fbd948cc7479
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-19 07:49:32 +0200

    First commit.

---
+++ Exercises/Properties/06 Vattenglas/Program.cs
@@ -0,0 +1,12 @@
+﻿// 6.Vattenglas
+// Skriv en klass som representerar ett vattenglas. Den ska ha en metod för att fylla glaset, 
+// och en metod för att tömma glaset. Dessutom ska den ha en private field som håller 
+// reda på om glaset är tomt eller fullt. Beroende på tillståndet (tomt/fullt) så ska 
+// metoden som fyller glaset skriva ut antingen “Fyller glaset” eller “Glaset är redan 
+// fullt”. Den andra metoden ska skriva ut “Tömmer glaset” eller “Glaset är redan tomt”.
+
+// Extra övning: Lägg till ytterligare en metod för att slå sönder glaset. Varje glas (instans) 
+// ska hålla reda på sitt tillstånd, och skriva ut vad som händer när man kör de olika 
+// metoderna. (t.ex “Glaset går sönder, och vattnet rinner ut på golvet”, eller “Glaset 
+// kan inte fyllas, eftersom det är trasigt” osv.)
+

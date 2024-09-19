1d517db78836d9ae0ae541cb0ed0c2ca87aa0cfc
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-19 07:28:47 +0200

    First commit.

---
+++ Exercises/Properties/05 Bil/Program.cs
@@ -0,0 +1,11 @@
+﻿// 5.Bil
+// Skriv en ny klass som motsvarar en bil. Den ska ha privata fields för modell, 
+// pris och färg. Skapa publika properties för att hämta eller ändra värdet på varje field.
+
+// Skriv en konstruktor till bilklassen som inte tar några parametrar. Skriv en till 
+// konstruktor som tar en parameter för varje property som klassen har och initierar 
+// dessa. Hur kan man styra vilken konstruktor som anropas när man skapar ett objekt av klassen?
+
+// Skriv en metod till bilklassen med namnet HalfPrice(). När den anropas ska priset 
+// på bilen sänkas till hälften.
+

ca8a99c95c32f3330d0907aa5be14efe7e7b499f
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-19 07:12:24 +0200

    Complete exercise 02.

---
+++ Exercises/Properties/02 AutoProp LastName/Program.cs
@@ -0,0 +1,23 @@
+﻿// 2.Autoproperty LastName
+// Lägg till en publik auto-property LastName som går att både läsa och skriva.
+
+Person someDude = new();
+
+someDude.FirstName = "John";
+someDude.LastName = "Lebowsky";
+
+Console.WriteLine($"{someDude.FirstName} {someDude.LastName}");
+
+
+class Person
+{
+    private string _firstName;
+
+    public string FirstName
+    {
+        get { return _firstName; }
+        set { _firstName = value; }
+    }
+    public string LastName { get; set; }
+
+}

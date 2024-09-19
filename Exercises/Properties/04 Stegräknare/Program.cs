6abf020d72de884b2ae75390681b30c183103874
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-19 07:19:04 +0200

    First commit.

---
+++ Exercises/Properties/04 Stegräknare/Program.cs
@@ -0,0 +1,8 @@
+﻿// 4.Stegräknare
+// Skapa en klass som kan användas som stegräknare. Den ska ha en property "Steps" som bara 
+// går att läsa; en metod Step() som räknar upp Steps med 1 varje gång man anropar den; och 
+// en metod Reset() som nollställer räknaren.
+
+// Instantiera klassen och skriv en loop som motsvarar att man går 1000 steg. 
+// Skriv ut värdet på Steps.
+

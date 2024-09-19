2828939e1cacc90d98d90849e81d32be85f49717
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-19 07:05:34 +0200

    Complete exercise 01.

--- Exercises/Properties/01 Person med Prop FirstName/Program.cs
+++ Exercises/Properties/01 Person med Prop FirstName/Program.cs
@@ -3,3 +3,21 @@
 // När man sätter värdet på FirstName ska detta sparas i _firstName, och när man läser värdet på 
 // FirstName ska den returnera det som finns lagrat i _firstName.
 
+Person someDude = new();
+
+someDude.FirstName = "Lebowky";
+
+Console.WriteLine(someDude.FirstName);
+
+
+class Person 
+{
+    private string _firstName;
+
+	public string FirstName
+	{
+		get { return _firstName; }
+		set { _firstName = value; }
+	}
+
+}

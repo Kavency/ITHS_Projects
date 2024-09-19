ad50479bc45548d05d63b65397e71c831131491c
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-19 07:00:33 +0200

    First commit.

---
+++ Exercises/Properties/01 Person med Prop FirstName/Program.cs
@@ -0,0 +1,5 @@
+﻿// 1.Person med property FirstName
+// Skapa en klass "Person" som har en private field _firstName, och en public property FirstName.
+// När man sätter värdet på FirstName ska detta sparas i _firstName, och när man läser värdet på 
+// FirstName ska den returnera det som finns lagrat i _firstName.
+

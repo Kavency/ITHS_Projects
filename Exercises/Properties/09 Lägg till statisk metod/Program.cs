01e6aecae83dababefd4dadd7141e254635d9121
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-20 07:30:23 +0200

    First commit.

---
+++ Exercises/Properties/09 Lägg till statisk metod/Program.cs
@@ -0,0 +1,5 @@
+﻿// 9.Lägg till statisk metod
+// Lägg till en statisk metod på klassen i uppgift 8. Metoden ska ta en 
+// bil som inparameter och returnera en array med 10 bilar i samma färg 
+// som bilen man skickat in, men med olika längd.
+

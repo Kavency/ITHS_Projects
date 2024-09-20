72ea2f10b68750e0144b86a64c21bce320b8f3bf
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-20 06:54:26 +0200

    Create class.

---
+++ Exercises/Properties/07 Blå Röd/Program.cs
@@ -0,0 +1,24 @@
+﻿// 7.Blå & Röd
+// Skapa en en klass som har en property “Red” och en property “Blue”. 
+// Båda ska vara en double och kunna ha ett värde mellan 0.0 och 100.0 
+// men de ska vara “sammankopplade” på så vis att värdena tillsammans 
+// alltid är 100.0 d.v.s om man t.ex. sätter “Blue” till 8.5 och sedan 
+// läser av “Red” så ska den returnera 91.5.
+
+
+
+class ColourConnection
+{
+    private double _blue = 50;
+    private double _red = 50;
+    private double _maxValue = 100;
+
+    public double Blue { set { _blue = value; _red = _maxValue - value; } }
+    public double Red { set { _red = value; _blue = _maxValue - value; } }
+    
+    void PrintValues()
+    {
+        Console.WriteLine($"Blue is:\t {_blue}");
+        Console.WriteLine($"Red is:\t {_red}");
+    }
+}

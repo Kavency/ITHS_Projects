927b8d74901399b6156fc060c570239e68e46832
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-23 20:58:14 +0200

    First commit.

---
+++ Exercises/Inheritance/03 Skapa en subklass för bilar/CarBrands.cs
@@ -0,0 +1,13 @@
+﻿// 3.Skapa en subklass för bilar
+// Skapa en klass Car som ärver av Vehicle. Lägg till en 
+// publik property Model, och skapa en konstruktor som 
+// initierar Brand, Model och Color.
+
+enum CarBrands
+{
+    Volvo,
+    Saab,
+    Toyota,
+    Kia,
+    Volkswagen
+}
---
+++ Exercises/Inheritance/03 Skapa en subklass för bilar/Program.cs
@@ -0,0 +1,7 @@
+﻿// 3.Skapa en subklass för bilar
+// Skapa en klass Car som ärver av Vehicle. Lägg till en 
+// publik property Model, och skapa en konstruktor som 
+// initierar Brand, Model och Color.
+
+Console.WriteLine(new Vehicle(CarBrands.Toyota, CarColours.White));
+

51609e50948047e536cac5ffda5dca00b5a89985
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-23 21:13:41 +0200

    Complete exercise 03.

--- Exercises/Inheritance/03 Skapa en subklass för bilar/Program.cs
+++ Exercises/Inheritance/03 Skapa en subklass för bilar/Program.cs
@@ -3,5 +3,65 @@
 // publik property Model, och skapa en konstruktor som 
 // initierar Brand, Model och Color.
 
-Console.WriteLine(new Vehicle(CarBrands.Toyota, CarColours.White));
+Console.WriteLine(new Car(CarBrands.Toyota, CarColours.White, "Yaris"));
+
+
+
+
+class Vehicle
+{
+    public CarBrands CarBrand { get; set; }
+    public CarColours CarColour { get; set; }
+
+    public Vehicle() { }
+    public Vehicle(CarBrands carBrand)
+    {
+        CarBrand = carBrand;
+        CarColour = CarColours.White;
+    }
+    public Vehicle(CarBrands carBrand, CarColours carColour)
+    {
+        CarBrand = carBrand;
+        CarColour = carColour;
+    }
+
+    public override string ToString()
+    {
+        return $"A {CarColour} {CarBrand}";
+    }
+}
+
+class Car : Vehicle
+{
+    public string CarModel { get; set; }
+
+    public Car(CarBrands carBrand, CarColours carColour, string carModel)
+    {
+        CarBrand = carBrand;
+        CarColour = carColour;
+        CarModel = carModel;
+    }
+
+    public override string ToString()
+    {
+        return $"A {CarColour} {CarModel} from {CarBrand}";
+    }
+}
+
+enum CarBrands
+{
+    Volvo,
+    Saab,
+    Toyota,
+    Kia,
+    Volkswagen
+}
+enum CarColours
+{
+    Blue,
+    White,
+    Red,
+    Green,
+    Gray
+}
 

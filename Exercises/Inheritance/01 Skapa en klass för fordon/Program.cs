1b986145a282fb78277728e33ecf0d512f04891a
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-23 20:18:41 +0200

    First commit.

---
+++ Exercises/Inheritance/01 Skapa en klass för fordon/Program.cs
@@ -0,0 +1,17 @@
+﻿// 1.Skapa en klass för fordon
+// Skapa en enum Brand med 5 bilmärken, och en enum Color med 5 färger.
+// Skriv sedan en klass Vehicle som har publika properties Brand och 
+// Color. Klassen ska ha en konstruktor som man kan använda för att 
+// sätta Brand och Color, och en konstruktor som endast tar Brand och 
+// sätter en default färg.
+
+namespace _01_Skapa_en_klass_för_fordon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

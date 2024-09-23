1b64025a18cc1351ef0dfd4ee916c4e1e366522e
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-23 21:17:06 +0200

    First commit.

---
+++ Exercises/Inheritance/04 Skapa en struct för längd bredd höjd/Program.cs
@@ -0,0 +1,5 @@
+﻿// 4.Skapa en struct för att spara längd, bredd och höjd.
+// Skapa en struct Size med properties Length, Width och Height; 
+// samt en konstruktor som tar 3 argument för att initiera dessa.
+
+

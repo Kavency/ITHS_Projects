2c24ce263d82b6120eeb159f2e66e14e53ae2806
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-24 17:53:57 +0200

    First commit.

---
+++ Exercises/Inheritance/07 Square ärver från Shape/Program.cs
@@ -0,0 +1,14 @@
+﻿// 7.Square ärver också från Shape
+// Skapa även en klass Square som ärver från Shape.
+// Implementera Square på så vis att koden nedan ...
+//
+// var square = new Square(5);
+// Console.WriteLine(square);
+// Console.WriteLine($"Area: {square.Area:f2}");
+// Console.WriteLine($"Circumference: {square.Circumference:f2}");
+// ... ger följande utskrift:
+//
+// A square with side 5
+// Area: 25,00
+// Circumference: 20,00
+

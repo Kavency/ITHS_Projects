2bcfb119624e7df59a639912490607f9502612a8
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-24 20:29:03 +0200

    First commit.

---
+++ Exercises/Inheritance/11 Lägg till färger i PrintAll/Program.cs
@@ -0,0 +1,4 @@
+﻿// 11.Lägg till färger i PrintAll()
+// Uppdatera PrintAll() i uppgift 10, så att alla rader med cirklar skrivs 
+// med röd färg och alla rader med kvadrater skrivs med grön färg.
+

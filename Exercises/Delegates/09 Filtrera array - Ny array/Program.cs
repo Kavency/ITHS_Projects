84d17c82e398ae1d9dc5d9057c6984ef98f11799
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-15 16:36:50 +0200

    Complete exercise 09.

--- Exercises/Delegates/09 Filtrera array - Ny array/Program.cs
+++ Exercises/Delegates/09 Filtrera array - Ny array/Program.cs
@@ -3,3 +3,38 @@
 // skriva ut talen returnerar en ny array med de tal
 // som matchar villkoren i lambdautrycket man skickar in.
 
+int[] numbers = { 2, -5, 34, 23, 87, 7, -95, 56, -32, 14 };
+
+Console.WriteLine("Negative numbers from new array:");
+int[] negativeNumbers = CheckNumbers(numbers, x => x < 0);
+foreach (var item in negativeNumbers)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("Numbers between 10 an 30 from new array:");
+int[] betweenNumbers = CheckNumbers(numbers, x => x > 10 && x < 30);
+foreach (var item in betweenNumbers)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("Even numbers from new array:");
+int[] evenNumbers = CheckNumbers(numbers, x => x % 2 == 0);
+foreach (var item in evenNumbers)
+{
+    Console.WriteLine(item);
+}
+
+int[] CheckNumbers(int[] numbers, Func<int, bool> func)
+{
+    List<int> resultList = new();
+    foreach (int number in numbers)
+    {
+        if (func(number))
+        {
+            resultList.Add(number);
+        }
+    }
+    return resultList.ToArray();
+}

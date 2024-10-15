2123ef6d606bae6e9c4054c81201db2da17d06f2
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-15 13:31:36 +0200

    Complete exercise 03.

--- Exercises/Delegates/03 Metod med delegat som inparameter/Program.cs
+++ Exercises/Delegates/03 Metod med delegat som inparameter/Program.cs
@@ -5,5 +5,25 @@
 // Skriv sedan en metod som tar ett generiskt delegat (som matchar metoden ovan) 
 // som inparameter. Metoden ska, i tur och ordning, anropa delegatet med 3 olika
 // för- och efternamn och skriva ut resultatet. Testa att anropa denna metod först
-// med en referens till metoden i uppgift 1 och sedan med metoden ovan (uppgift 3).
+// med en referens till metoden som i uppgift 1 och sedan med metoden ovan (uppgift 3).
 
+Func<string, string, string> CallNames;
+CallNames = GetName;
+
+Console.WriteLine(CallNames("First", "Call"));
+
+CallDelegate(CallNames);
+
+
+void CallDelegate(Func<string, string, string> nameCaller)
+{
+    Console.WriteLine(CallNames("John", "Doe"));
+    Console.WriteLine(CallNames("Jane", "Doe"));
+    Console.WriteLine(CallNames("Some Other", "Doe"));
+    //Console.WriteLine(nameCaller);
+}
+
+string GetName(string firstName, string lastName)
+{
+    return $"{firstName}\r\n -{lastName}";
+}

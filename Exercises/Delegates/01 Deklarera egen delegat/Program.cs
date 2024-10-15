13dc9899e6d02e447723275241bedbff661098f0
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-15 06:53:46 +0200

    Complete exercise 01.

--- Exercises/Delegates/01 Deklarera egen delegat/Program.cs
+++ Exercises/Delegates/01 Deklarera egen delegat/Program.cs
@@ -5,11 +5,21 @@
 // metoden. Anropa metoden via delegatet.
 
 
+// 03 - Instantiate the delegate with a method with the same signature, no paranthesis. This
+//      is not a method call.
+FullName personsFullName = GetFullName;
 
+
+// 04 - Call the delegate.
+Console.WriteLine(personsFullName("John", "Doe"));
+
+
+// 02 - Method with a signature that can be used by the delegate.
 string GetFullName(string firstName, string lastName) 
 {
     return $"{firstName} {lastName}";
 }
 
 
+// 01 - Declare a delegate with a specific method signature.
 public delegate string FullName(string firstName, string lastName);

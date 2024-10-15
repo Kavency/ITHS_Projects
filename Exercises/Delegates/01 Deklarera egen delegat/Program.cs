9b580ff3ba4e62bd019d07dbef87776715dbbcbb
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-15 06:36:53 +0200

    Add instructions in comment

--- Exercises/Delegates/01 Deklarera egen delegat/Program.cs
+++ Exercises/Delegates/01 Deklarera egen delegat/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1.Deklarera en egen delegat-typ
+// Skriv en metod som tar två strängar: firstName och lastName.
+// Metoden ska returnera en ny sträng som är hela namnet. Deklarera 
+// en delegat-typ, och gör en variabel som är en referens till 
+// metoden. Anropa metoden via delegatet.
+

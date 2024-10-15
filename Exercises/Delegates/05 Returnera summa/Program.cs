d1ad2e9dfcb4c141d6e4b11ee2957f2f5a6e9dfa
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-15 14:10:27 +0200

    Add project 05.

---
+++ Exercises/Delegates/05 Returnera summa/Program.cs
@@ -0,0 +1,6 @@
+﻿// 5.Returnera summan av två värden
+// Skapa ett generiskt delegat med ett lambda uttryck som tar 
+// två tal och returnerar summan av dessa. Testa genom att
+// anropa med två värden, och skriva ut resultatet.
+
+// Declare generic delegate.   - Assign a Lambda.

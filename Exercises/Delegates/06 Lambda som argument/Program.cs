64a7707e221381094e672631fed81afbdc0e91a5
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-15 14:16:14 +0200

    Add project 06.

---
+++ Exercises/Delegates/06 Lambda som argument/Program.cs
@@ -0,0 +1,6 @@
+﻿// 6.Lambda som argument
+// Anropa metoden (den som tar ett generiskt delegat) i uppgift 3. 
+// Som argument skriver du ett lambdauttryck som returnerar en 
+// string med antal tecken i namnen: 
+// Ex. "firstName has 7 letters, lastName has 9 letters."
+

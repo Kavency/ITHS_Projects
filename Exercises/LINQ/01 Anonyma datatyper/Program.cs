127a4cd30b1fe3bac66ad12c8ec791c7170625da
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-15 16:43:37 +0200

    Add project 01.

---
+++ Exercises/LINQ/01 Anonyma datatyper/Program.cs
@@ -0,0 +1,8 @@
+﻿// 1.Anonyma datatyper
+// Skapa en array "people" med anonyma datatyper. Varje objekt i 
+// arrayen ska ha följande properties: FirstName, LastName, Age, Height, Weight.
+//
+// Lägg till minst 8 sådana objekt i arrayen.
+//
+// Använd dig sedan av LINQ för att lösa följande uppgifter:
+

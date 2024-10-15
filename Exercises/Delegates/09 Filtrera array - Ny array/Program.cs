5236529c5ec85c63e16be77f9bc82455655adff6
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-15 16:28:42 +0200

    Add project 09.

---
+++ Exercises/Delegates/09 Filtrera array - Ny array/Program.cs
@@ -0,0 +1,5 @@
+﻿// 9.Filtrera array(till ny array)
+// Gör om metoden i uppgift 8 så att den istället för att
+// skriva ut talen returnerar en ny array med de tal
+// som matchar villkoren i lambdautrycket man skickar in.
+

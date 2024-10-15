780a721737eca368bd077e10f488f6d63388da78
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-15 14:48:35 +0200

    Add project 07.

---
+++ Exercises/Delegates/07 Modifiera utskrift med lambdauttryck/Program.cs
@@ -0,0 +1,14 @@
+﻿// 7.Modifiera utskrift med lambdauttryck
+// Skriv en metod som tar en string[] som inparameter och skriver
+// ut alla strängar i arrayen på varsin rad. Uppdatera sedan metoden
+// så att den tar en andra parameter: Func<string, string> modifier,
+// och anropa modifier() på varje string i arrayen innan du skriver ut den.
+//
+// Skapa en en string[] med namnen på fem städer som du kan skicka in i metoden.
+//
+// Gör 3 anrop till metoden (med olika lambda-uttryck):
+//
+// Ett som skriver ut alla städer i UPPERCASE
+// Ett som skriver ut de 3 första tecknen i varje stad
+// Ett som skriver ut längden på varje stads namn.
+

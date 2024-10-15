3f9ed9c71cade436f2e679cda01c47cdcbd62e68
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-15 07:49:07 +0200

    Add eexercise 03.

---
+++ Exercises/Delegates/03 Metod med delegat som inparameter/Program.cs
@@ -0,0 +1,9 @@
+﻿// 3.Metod som tar delegat som inparameter
+// Skriv en ny metod som också tar firstName och lastName. Denna metoden ska
+// returnera en sträng som innehåller variabelnamnen och dess värden på två rader.
+//
+// Skriv sedan en metod som tar ett generiskt delegat (som matchar metoden ovan) 
+// som inparameter. Metoden ska, i tur och ordning, anropa delegatet med 3 olika
+// för- och efternamn och skriva ut resultatet. Testa att anropa denna metod först
+// med en referens till metoden i uppgift 1 och sedan med metoden ovan (uppgift 3).
+

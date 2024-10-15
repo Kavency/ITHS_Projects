c167efbc78c93b3cc2306e34c531a31e2e323300
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-15 14:43:22 +0200

    Complete exercise 06.

--- Exercises/Delegates/06 Lambda som argument/Program.cs
+++ Exercises/Delegates/06 Lambda som argument/Program.cs
@@ -4,3 +4,16 @@
 // string med antal tecken i namnen: 
 // Ex. "firstName has 7 letters, lastName has 9 letters."
 
+Func<string, string, string> CallNames;
+
+//CallNames = GetName;
+CallNames = (x, y) => $"Firstname is {x.Length} characters long and the lastname is {y.Length} long.";
+
+CallDelegate(CallNames);
+
+void CallDelegate(Func<string, string, string> nameCaller)
+{
+    Console.WriteLine(nameCaller("John", "Doe"));
+    Console.WriteLine(nameCaller("Jane", "Doe"));
+    Console.WriteLine(nameCaller("Some Other", "Doe"));
+}

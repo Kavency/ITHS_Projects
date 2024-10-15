c4d84e93211d4281c0c9009295e460df93e54134
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-15 17:16:13 +0200

    Add project 05.

---
+++ Exercises/LINQ/05 Namnet med BMI/Program.cs
@@ -0,0 +1,5 @@
+﻿// 5.Hela namnet tillsammans med BMI
+// Skapa en ny lista från arrayen (från uppgift 1) som innehåller 
+// objekt med properties Name (hela namnet), samt BMI 
+// (uträknat från längd och vikt) för alla personer.
+

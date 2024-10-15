0579451be13c2b5698c2d0d107ae62654a1a1820
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-15 17:12:08 +0200

    Add project 04.

---
+++ Exercises/LINQ/04 Filtrera på namn/Program.cs
@@ -0,0 +1,6 @@
+﻿// 4.Filtrera på namn
+// Filtrera arrayen från uppgift 1 så att du får en ny lista med 
+// endast de användare vars förnamn är längre än efternamnet
+// (d.v.s större antal bokstäver). I den nya listan vill vi att 
+// objekten ska ha endast properties FirstName och LastName.
+

3bdf1a66e8544fbbee17e49dfa70d34a01e0fe52
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-15 13:45:15 +0200

    Add project 04.

---
+++ Exercises/Delegates/04 Summan av två värden/Program.cs
@@ -0,0 +1,4 @@
+﻿// 4.Skriv ut summan av två värden
+// Skapa ett generiskt delegat med ett lambda uttryck 
+// som tar två tal och skriver ut summan av dessa på 
+// konsolen. Testa genom att anropa med två värden.

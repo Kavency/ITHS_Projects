ab0d462a4cefd63d1ceae5ab41ef16552d09c70a
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-15 07:45:45 +0200

    Complete exercise 02.

--- Exercises/Delegates/02 Använd generiskt delegat/Program.cs
+++ Exercises/Delegates/02 Använd generiskt delegat/Program.cs
@@ -2,3 +2,13 @@
 // Ändra koden i uppgift 01 så att den använder ett generiskt
 // delegat istället för den du själv deklarerade.
 
+Func<string, string, string> fullName;
+
+fullName = GetFullName;
+
+Console.WriteLine(fullName("John", "Doe"));
+
+string GetFullName(string firstName, string lastName)
+{
+    return $"{firstName} {lastName}";
+}

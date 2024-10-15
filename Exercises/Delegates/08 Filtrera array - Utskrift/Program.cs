ed9672391f97e2f5878c89f997cc85b897feb454
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-15 16:15:33 +0200

    Add project 08.

---
+++ Exercises/Delegates/08 Filtrera array - Utskrift/Program.cs
@@ -0,0 +1,14 @@
+﻿// 8.Filtrera array(utskrift)
+// Skapa en metod som tar en array med heltal, samt ett delegat som tar 
+// en int som inparameter och returnerar en bool. Metoden ska sedan anropa 
+// delegatet för varje tal i arrayen och endast skriva ut de tal som 
+// returnerar true.
+//
+// Skapa en int[] med 10 tal som du kan skicka in i metoden:
+//
+// Gör 3 anrop till metoden:
+//
+// Ett som listar alla negativa tal
+// Ett som listar alla tal mellan 10 och 20
+// Ett som listar alla jämna tal
+

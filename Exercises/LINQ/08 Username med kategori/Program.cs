d7b8fbd12e5d6f8a8b82192b43a9e67d91a3b528
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-17 20:09:40 +0200

    Add project 08.

---
+++ Exercises/LINQ/08 Username med kategori/Program.cs
@@ -0,0 +1,6 @@
+﻿// 8.Username tillsammans med Category
+// Skapa en ny lista från arrayen (i uppgift 1) som innehåller objekt
+// med properties Username (förnamnet + åldern, ex. “Kalle23”), 
+// samt Category (som har värdet “Child” eller “Adult” beroende 
+// på om personen är under 18 eller inte).
+

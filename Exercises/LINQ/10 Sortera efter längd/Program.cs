6a8be1ad1b601b86b31c355f2e14d905e19f8877
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-17 21:35:36 +0200

    Add project 10.

---
+++ Exercises/LINQ/10 Sortera efter längd/Program.cs
@@ -0,0 +1,5 @@
+﻿// 10.Sortera efter längd
+// LINQ använder metoderna .OrderBy() och .OrderByDescending() för att sortera data.
+//
+// Skriv ut alla personer (från uppgift 1) i längdordning (kortast först).
+

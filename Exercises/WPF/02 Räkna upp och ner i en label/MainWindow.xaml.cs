89ec6cd16f43af6c018a0a3f2e8176d6b953eeb3
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-18 16:42:07 +0200

    Add comment

--- Exercises/WPF/02 Räkna upp och ner i en label/MainWindow.xaml.cs
+++ Exercises/WPF/02 Räkna upp och ner i en label/MainWindow.xaml.cs
@@ -1,13 +1,12 @@
-﻿using System.Text;
+﻿// 2.Räkna upp och ner i en label.
+// Gör ett program med en label som initialt visar 
+// värdet 5. Det ska finna en knapp "Increase" och 
+// en knapp "Decrease". Genom att klicka på knapparna 
+// ska man kunna öka eller minska värdet som visas på 
+// label, ett steg i taget. Värdet ska dock aldrig kunna 
+// bli lägre än 0, eller högre än 9.
+
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace _02_Räkna_upp_och_ner_i_en_label
 {

f1f0ccf34db674e45d1b7929c55915ba5073b5ae
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-18 09:22:32 +0200

    Complete exercise 12.

--- Exercises/LINQ/12 Sortering i första och andra hand/Program.cs
+++ Exercises/LINQ/12 Sortering i första och andra hand/Program.cs
@@ -7,3 +7,22 @@
 // i stigande ordning (A -> Z). Om flera personer har samma efternamn, 
 // sortera dessa på förnamn i fallande ordning (Z -> A).
 
+var people = new[] {
+      new { LastName = "Eriksson",  FirstName = "Anders", Age = 39, Height = 1.82, Weight = 85 },
+      new { LastName = "Palm", FirstName = "Sandra",  Age = 40, Height = 1.98, Weight = 92 },
+      new { LastName = "Andersson",  FirstName = "Per",  Age = 17, Height = 1.92, Weight = 87 },
+      new { LastName = "Lundberg",  FirstName = "Anna",  Age = 66, Height = 1.67, Weight = 74 },
+      new { LastName = "Eriksson",  FirstName = "Camilla",  Age = 65, Height = 1.32, Weight = 64 },
+      new { LastName = "Eriksson",  FirstName = "Mats",  Age = 65, Height = 1.32, Weight = 64 },
+      new { LastName = "Eriksson",  FirstName = "Olof",  Age = 65, Height = 1.32, Weight = 64 },
+      new { LastName = "Eriksson",  FirstName = "Vanessa",  Age = 65, Height = 1.32, Weight = 64 },
+      new { LastName = "Svensson",  FirstName = "Erika",  Age = 47, Height = 1.78, Weight = 84 },
+      new { LastName = "Olofsson",  FirstName = "Sven",  Age = 25, Height = 1.67, Weight = 74 },
+      new { LastName = "Karlsson",  FirstName = "Per Olof Stekare",  Age = 12, Height = 1.94, Weight = 87 } };
+
+var sorted = people.OrderBy(p => p.LastName).ThenBy(p => p.FirstName);
+
+foreach (var item in sorted)
+{
+    Console.WriteLine($"{item.FirstName} {item.LastName}");
+}

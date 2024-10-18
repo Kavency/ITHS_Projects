d9af1806478e259480a2b62c6762e98db2445f46
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-18 09:14:34 +0200

    Complete exercise 10.

--- Exercises/LINQ/10 Sortera efter längd/Program.cs
+++ Exercises/LINQ/10 Sortera efter längd/Program.cs
@@ -3,3 +3,19 @@
 //
 // Skriv ut alla personer (från uppgift 1) i längdordning (kortast först).
 
+var people = new[] {
+      new { LastName = "Eriksson",  FirstName = "Anders", Age = 39, Height = 1.82, Weight = 85 },
+      new { LastName = "Palm", FirstName = "Sandra",  Age = 40, Height = 1.98, Weight = 92 },
+      new { LastName = "Andersson",  FirstName = "Per",  Age = 17, Height = 1.92, Weight = 87 },
+      new { LastName = "Lundberg",  FirstName = "Anna",  Age = 66, Height = 1.67, Weight = 74 },
+      new { LastName = "Eriksson",  FirstName = "Camilla",  Age = 65, Height = 1.32, Weight = 64 },
+      new { LastName = "Svensson",  FirstName = "Erika",  Age = 47, Height = 1.78, Weight = 84 },
+      new { LastName = "Olofsson",  FirstName = "Sven",  Age = 25, Height = 1.67, Weight = 74 },
+      new { LastName = "Karlsson",  FirstName = "Per Olof Stekare",  Age = 12, Height = 1.94, Weight = 87 } };
+
+var sorted = people.OrderBy(p => p.Height);
+
+foreach (var item in sorted)
+{
+    Console.WriteLine($"{item.FirstName} {item.LastName} - {item.Height}");
+}

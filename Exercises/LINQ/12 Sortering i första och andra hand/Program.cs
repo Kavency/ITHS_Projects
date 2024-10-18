3be01f102adce7717a0d7b6c23a31df9ebc00b90
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-18 09:18:48 +0200

    Add project 12.

---
+++ Exercises/LINQ/12 Sortering i första och andra hand/Program.cs
@@ -0,0 +1,9 @@
+﻿// 12.Sortering i första och andra hand
+// Efter .OrderBy eller .OrderByDescending() kan man använda .ThenBy() 
+// och .ThenByDescending() för att sortera på något annat i andra hand 
+// (om flera objekt har samma värden i första sorteringen).
+//
+// Skriv ut alla personer (från uppgift 1) sorterade på efternamn 
+// i stigande ordning (A -> Z). Om flera personer har samma efternamn, 
+// sortera dessa på förnamn i fallande ordning (Z -> A).
+

027118b6d9b4af6f38943c9cd35a4de6dfeae76a
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-18 10:51:21 +0200

    Add project 02.

---
+++ Exercises/XAML/02 Knappar i hörnen/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _02_Knappar_i_hörnen
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}

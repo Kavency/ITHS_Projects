1a1b680d82657c7e71e801e94bf2cf2135713d0e
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-18 09:24:52 +0200

    Add project 13.

---
+++ Exercises/LINQ/13 LINQ vs PLINQ/Program.cs
@@ -0,0 +1,15 @@
+﻿// 13.LINQ vs PLINQ
+// Använd Enumerable.Range() för att skapa en sekvens av tal 
+// från 1 upp till 1 miljard. Filtrera sedan sekvensen på så 
+// vis att du får endast de tal som är jämnt delbara med 3 
+// eller 5. Beräkna summan av talen.
+//
+// OBS! Använd inte ToList() ovan, då detta till skillnad från 
+// Enumerable faktiskt skapar upp alla tal i minnet.
+//
+// Sätt breakpoints i din kod och använd debuggern för 
+// att kolla hur lång tid det tar att exekvera.
+//
+// Prova sedan att köra samma beräkning som parallell LINQ; 
+// jämför tiden det tar att exekvera.
+

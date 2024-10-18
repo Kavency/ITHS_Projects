4b353257c55c3f3e86a54101e32a93153856f4f0
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-18 09:17:28 +0200

    Complete exercise 11.

--- Exercises/LINQ/11 Sortera äldst till yngst/Program.cs
+++ Exercises/LINQ/11 Sortera äldst till yngst/Program.cs
@@ -2,3 +2,19 @@
 // Skriv ut alla personer (från uppgift 1) sortera
 // efter ålder; från äldst till yngst.
 
+var people = new[] {
+      new { LastName = "Eriksson",  FirstName = "Anders", Age = 39, Height = 1.82, Weight = 85 },
+      new { LastName = "Palm", FirstName = "Sandra",  Age = 40, Height = 1.98, Weight = 92 },
+      new { LastName = "Andersson",  FirstName = "Per",  Age = 17, Height = 1.92, Weight = 87 },
+      new { LastName = "Lundberg",  FirstName = "Anna",  Age = 66, Height = 1.67, Weight = 74 },
+      new { LastName = "Eriksson",  FirstName = "Camilla",  Age = 65, Height = 1.32, Weight = 64 },
+      new { LastName = "Svensson",  FirstName = "Erika",  Age = 47, Height = 1.78, Weight = 84 },
+      new { LastName = "Olofsson",  FirstName = "Sven",  Age = 25, Height = 1.67, Weight = 74 },
+      new { LastName = "Karlsson",  FirstName = "Per Olof Stekare",  Age = 12, Height = 1.94, Weight = 87 } };
+
+var sorted = people.OrderByDescending(p => p.Age);
+
+foreach (var item in sorted)
+{
+    Console.WriteLine($"{item.FirstName} {item.LastName} - {item.Age}");
+}

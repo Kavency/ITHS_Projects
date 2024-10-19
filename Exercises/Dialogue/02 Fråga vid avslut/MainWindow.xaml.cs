337c212176b3ccf4b8b1dc27c0f154691a59c735
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-19 14:03:08 +0200

    Add project 02.

---
+++ Exercises/Dialogue/02 Fråga vid avslut/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿// 2.Fråga vid avslut
+// Lyssna på fönstrets "Closing"-event och visa en MessageBox med frågan
+// "Are you sure you want to quit?". Om användaren väljer "No", avbryt 
+// stängning av fönstret genom att sätta Cancel = true på 
+// CancelEventArgs-objektet som skickas med när eventet triggas.
+
+using System.Windows;
+
+namespace _02_Fråga_vid_avslut
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

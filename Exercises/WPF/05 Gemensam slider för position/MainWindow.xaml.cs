cc10f097ff4394c3538acfac1e48c64de8ee3f33
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-19 10:50:26 +0200

    Add project 05.

---
+++ Exercises/WPF/05 Gemensam slider för position/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿// 5.Gemensam slider för position
+// Gör en ny version av programmet i uppgift 4, där det nya
+// programmet bara har en slider för att välja värdet. Både 
+// x och y ska sättas till värdet man valt på slidern (0-100).
+//
+// Det ska också finnas två checkboxar. En "Lock x-value." och en 
+// "Lock y-value". Dessa ska kunna låsa x- och y-värden från att
+// uppdateras. D.v.s om man t.ex klickar i "Lock x-value" och sedan 
+// ändrar värdet på slidern så ska knappens y-värde fortfarande uppdateras, 
+// medan x-värdet uppdateras först när man klickar ur checkboxen igen.
+//
+// Förtydligande: Om man klickar i båda checkboxar så ska positionen 
+// inte uppdateras alls när man ändrar värdet, förrän man klickar 
+// ur checkboxarna: då "hoppar" labeln till sin nya position.
+
+using System.Windows;
+
+namespace _05_Gemensam_slider_för_position
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

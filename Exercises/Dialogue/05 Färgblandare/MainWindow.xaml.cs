409f656a5b9a4f6b2758a5b673cb59e237f8fea0
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-20 21:21:34 +0200

    Create ColourPicker window with content.

--- Exercises/Dialogue/05 Färgblandare/MainWindow.xaml.cs
+++ Exercises/Dialogue/05 Färgblandare/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace _05_Färgblandare
 {
@@ -19,6 +10,12 @@ namespace _05_Färgblandare
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void chooseBTN_Click(object sender, RoutedEventArgs e)
+        {
+            ColourPickerWindow colourPickerWindow = new();
+            colourPickerWindow.Show();
         }
     }
 }

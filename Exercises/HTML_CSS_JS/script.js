9310fa9bcb6b35825fe36ffdd8082e7ef7a62458
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-02-19 10:31:37 +0100

    Add files

--- Exercises/HTML_CSS_JS/script.js
+++ Exercises/HTML_CSS_JS/script.js
@@ -1,36 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-        var button = document.getElementById("myButton")
-        
+        var button = document.getElementById("myButton");
+        let label = document.getElementById("myLabel");
+
         button.addEventListener("click", () => {
            
-            document.body.style.backgroundColor = `rgb(${getRandomInt(0, 255)}, ${getRandomInt(0, 255)}, ${getRandomInt(0, 255)})`
-            button.style.backgroundColor = `rgb(${getRandomInt(0, 255)},${getRandomInt(0, 255)},${getRandomInt(0, 255)})`
-            button.style.color = `rgb(${getRandomInt(0, 255)},${getRandomInt(0, 255)},${getRandomInt(0, 255)})`
+            document.body.style.backgroundColor = `rgb(${getRandomInt()}, ${getRandomInt()}, ${getRandomInt()})`
+            button.style.backgroundColor = `rgb(${getRandomInt()},${getRandomInt()},${getRandomInt()})`
+            button.style.color = `rgb(${getRandomInt()},${getRandomInt()},${getRandomInt()})`
 
             button.textContent = randomizeText(button.textContent);
+
+            label.textContent++;
+            label.style.color = `rgb(${getRandomInt()},${getRandomInt()},${getRandomInt()})`;
+
         })
     })
 
     function randomizeText(text) {
-        // Separate the last character (exclamation mark) from the rest of the string
         let mainText = text.slice(0, -1);
         let lastChar = text.slice(-1);
-    
-        // Convert the main text to an array of characters
+
         let chars = mainText.split('');
     
-        // Shuffle the array of characters
         for (let i = chars.length - 1; i > 0; i--) {
             let j = Math.floor(Math.random() * (i + 1));
             [chars[i], chars[j]] = [chars[j], chars[i]];
         }
     
-        // Convert the array back to a string and append the last character
         return chars.join('') + lastChar;
     }
 
-    function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
+    function getRandomInt() {
+        let min = Math.ceil(0);
+        let max = Math.floor(255);
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }

73f09c6a92929265563bc5b5f66c637632234604
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-02-19 11:44:30 +0100

    Add references

--- Exercises/ExerciseBackEndDemo/CustomerService/Program.cs
+++ Exercises/ExerciseBackEndDemo/CustomerService/Program.cs
@@ -1,3 +1,14 @@
+using Models;
+
+List<Customer> customers = new List<Customer>
+{
+    new Customer(0, "Kalle"),
+    new Customer(1, "kajsa"),
+    new Customer(2, "Knatte"),
+    new Customer(3, "Fnatte"),
+    new Customer(4, "Tjatte")
+};
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
---
+++ Exercises/ExerciseBackEndDemo/Models/Booking.cs
@@ -0,0 +1,6 @@
+namespace Models;
+public class Booking
+{
+    public int Id { get; set; }
+    public required Customer Customer { get; set; }
+}

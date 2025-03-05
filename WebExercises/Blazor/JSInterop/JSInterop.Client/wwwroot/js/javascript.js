1412a65b5bd35193415eccafffefecece14cac9f
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2025-03-05 14:37:26 +0100

    Refactor

--- WebExercises/Blazor/JSInterop/JSInterop.Client/wwwroot/js/javascript.js
+++ WebExercises/Blazor/JSInterop/JSInterop.Client/wwwroot/js/javascript.js
@@ -1,1 +1,18 @@
-﻿
+﻿function changeColour() {
+    let min = 0;
+    let max = 255;
+
+    let r = Math.floor(Math.random() * (max - min + 1)) + min;;
+    let g = Math.floor(Math.random() * (max - min + 1)) + min;;
+    let b = Math.floor(Math.random() * (max - min + 1)) + min;;
+
+    document.body.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+};
+
+function getWindowHeight() {
+    return window.innerHeight;
+};
+
+function getWindowWidth() {
+    return window.innerWidth;
+};

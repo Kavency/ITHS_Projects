9e91e69a173413cb8011df28d0347281042e1776
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-11-14 14:55:55 +0100

    Add singleton

--- CleanCodeExercises/DesignPatterns/Creational/SingletonPattern/Program.cs
+++ CleanCodeExercises/DesignPatterns/Creational/SingletonPattern/Program.cs
@@ -14,21 +14,31 @@ internal class Program
         var nonSingletonLogger1 = new Logger();
         var nonSingletonLogger2 = new Logger();
 
-        Console.WriteLine("---------- Different instances ----------");
+        Console.WriteLine("---------- Non singleton ----------");
         
         nonSingletonLogger1.Log("This is not good.");
         nonSingletonLogger2.Log("This is even worse.");
 
         Console.WriteLine($"Same instance? {(ReferenceEquals(nonSingletonLogger1, nonSingletonLogger2) ? "Yes" : "No")}\n");
 
-        // Using a singleton
-        var logger1 = serviceProvider.GetService<ILogger>();
-        var logger2 = serviceProvider.GetService<ILogger>();
+        // Using a singleton through DI
+        var DiLogger1 = serviceProvider.GetService<ILogger>() ?? throw new Exception("No service found.");
+        var DiLogger2 = serviceProvider.GetService<ILogger>() ?? throw new Exception("No service found.");
 
-        Console.WriteLine("------------- Same instance -------------");
-        logger1.Log("Something happend.");
-        logger2.Log("Something else happend.");
+        Console.WriteLine("------------- DI Singleton -------------");
+        DiLogger1.Log("Something happend.");
+        DiLogger2.Log("Something else happend.");
 
-        Console.WriteLine($"Same instance? {(ReferenceEquals(logger1, logger2) ? "Yes" : "No")}\n");
+        Console.WriteLine($"Same instance? {(ReferenceEquals(DiLogger1, DiLogger2) ? "Yes" : "No")}\n");
+
+        // Using a manually created singleton
+        var ManualLogger1 = SingletonLogger.GetInstance;
+        var ManualLogger2 = SingletonLogger.GetInstance;
+
+        Console.WriteLine("------------- Manual Singleton -------------");
+        ManualLogger1.Log("Logging something.");
+        ManualLogger2.Log("Also logging.");
+
+        Console.WriteLine($"Same instance? {(ReferenceEquals(ManualLogger1, ManualLogger2) ? "Yes" : "No")}\n");
     }
 }

46953d0acee3fa3fcf46f07e4b1953fba1be56f3
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-11-14 11:50:50 +0100

    Add project

---
+++ CleanCodeExercises/DesignPatterns/Creational/SingletonPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SingletonPattern;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddSingleton<ILogger, Logger>();
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        var logger1 = serviceProvider.GetService<ILogger>();
+        var logger2 = serviceProvider.GetService<ILogger>();
+
+        logger1.Log("Something happend.");
+        logger2.Log("Something else happend.");
+
+        Console.WriteLine($"Same instance? {(ReferenceEquals(logger1, logger2) ? "Yes" : "No")}");
+    }
+}
